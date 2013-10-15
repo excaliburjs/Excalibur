e16d3bf3f79a83815eeef482fa4347fe591ce329
eonarheim <onarh001@umn.edu>
2013-10-14 22:04:10 -0500

    Fix monkeypatch bug where we use setTimeout instead of setInterval

--- ts/MonkeyPatch.ts
+++ ts/MonkeyPatch.ts
@@ -32,7 +32,7 @@ if(typeof window != 'undefined' && !window.requestAnimationFrame){
 	(<any>window).requestAnimationFrame = (<any>window).webkitRequestAnimationFrame ||
 								          (<any>window).mozRequestAnimationFrame    ||
 								          function( callback ){
-								            window.setTimeout(callback, 1000 / 60);
+								            window.setInterval(callback, 1000 / 60);
 								          };
 }
 
