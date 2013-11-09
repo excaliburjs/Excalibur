f15308980bf490ba0285500edea9bd32de03fce8
eonarheim <erik.onarheim@gmail.com>
2013-11-08 19:14:25 -0600

    Close #86 implement promises in Excalibur, added a Promise.wrap to wrap non promise values in a resolved promise

--- runTests.bat
+++ runTests.bat
@@ -1,4 +1,5 @@
 del "spec/*.js"
 call tsc "spec/ActorSpec.ts" -out "spec/ActorSpec.js"
 call tsc "spec/ColorSpec.ts" -out "spec/ColorSpec.js"
+call tsc "spec/PromiseSpec.ts" -out "spec/PromiseSpec.js"
 call jasmine-node --verbose spec/
