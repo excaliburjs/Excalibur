2290f8590913ac5502e77533230d1af30a9f9118
Josh Edeen <josh.edeen@gmail.com>
2013-11-12 13:22:39 -0600

    removing call to load old sample game location

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 call tsc --declaration ./ts/Core.ts -out ./build/Excalibur.js
 call tools\nuget pack Excalibur.nuspec -OutputDirectory ./build
 if %ERRORLEVEL% EQU 0 call tsc ./sample-game/ts/game.ts
-if %ERRORLEVEL% EQU 0 start ./sample-game/html/index.html
+::if %ERRORLEVEL% EQU 0 start ./sample-game/html/index.html
