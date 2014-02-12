7e1d36038bd99fa08ec8d7c5d8ace4881ea047f5
Kamran Ayub <kamran.ayub@gmail.com>
2014-02-11 21:57:42 -0600

    Add new specs to bat

--- runTests.bat
+++ runTests.bat
@@ -4,4 +4,6 @@ call tsc "spec/ColorSpec.ts" -out "spec/ColorSpec.js"
 call tsc "spec/PromiseSpec.ts" -out "spec/PromiseSpec.js"
 call tsc "spec/CollectionSpec.ts" -out "spec/CollectionSpec.js"
 call tsc "spec/LogSpec.ts" -out "spec/LogSpec.js"
+call tsc "spec/ClassSpec.ts" -out "spec/ClassSpec.js"
+call tsc "spec/TimerSpec.ts" -out "spec/TimerSpec.js"
 call jasmine-node --verbose spec/
