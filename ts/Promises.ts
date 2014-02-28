668756609a95e575ef75373d8ba7e686a6a778ed
eonarheim <erik.onarheim@gmail.com>
2014-02-28 11:50:49 -0600

    Fix promise logging

--- ts/Promises.ts
+++ ts/Promises.ts
@@ -27,6 +27,7 @@ module ex {
       private successCallbacks: { (value?: T): any }[] = [];
       private rejectCallback: (value?: any) => any = () => { };
       private errorCallback: (value?: any) => any = () => { };
+      private logger : Logger = Logger.getInstance();
 
       public static wrap<T>(value?: T): Promise<T> {
          var promise = (new Promise<T>()).resolve(value);
