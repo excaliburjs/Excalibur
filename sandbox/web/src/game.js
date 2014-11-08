af3f8ce05d8e61e36803681328d59ded05cbf187
Kamran Ayub <kamran.ayub@gmail.com>
2014-11-08 15:26:43 -0600

    Compile/finish merge

--- sandbox/web/src/game.js
+++ sandbox/web/src/game.js
@@ -59,6 +59,7 @@ game.backgroundColor = new ex.Color(114, 213, 224);
 
 // Turn on debug diagnostics
 game.isDebug = false;
+var blockSprite = new ex.Sprite(imageBlocks, 0, 0, 65, 49);
 
 // Create spritesheet
 var spriteSheetRun = new ex.SpriteSheet(imageRun, 21, 1, 96, 96);
@@ -135,7 +136,10 @@ game.add(follower);
 
 // Create the player
 var player = new ex.Actor(100, -200, 32, 96);
-follower.meet(player, 60);
+player.collisionType = 2 /* Active */;
+follower.meet(player, 60).asPromise().then(function () {
+    console.log("Player met!!");
+});
 
 // follow player
 player.scaleX = 1;
@@ -183,8 +187,7 @@ var groundSpeed = 150;
 var airSpeed = 130;
 var jumpSpeed = 500;
 var direction = 1;
-
-var moveLeft = function () {
+player.addEventListener('left', function () {
     direction = -1;
     if (!inAir) {
         player.setDrawing(2 /* Left */);
@@ -195,9 +198,9 @@ var moveLeft = function () {
     }
     player.dx = -groundSpeed;
     // TODO: When platform is moving in same direction, add its dx
-};
+});
 
-var moveRight = function () {
+player.addEventListener('right', function () {
     direction = 1;
     if (!inAir) {
         player.setDrawing(3 /* Right */);
@@ -208,11 +211,11 @@ var moveRight = function () {
     }
     player.dx = groundSpeed;
     // TODO: When platform is moving in same direction, add its dx
-};
+});
 
-var moveJump = function () {
+player.addEventListener('up', function () {
     if (!inAir) {
-        player.dy -= jumpSpeed;
+        player.dy = -jumpSpeed;
         inAir = true;
         if (direction === 1) {
             player.setDrawing(4 /* JumpRight */);
@@ -221,20 +224,7 @@ var moveJump = function () {
         }
         jumpSound.play();
     }
-};
-
-var resetToIdle = function (e) {
-    if (inAir)
-        return;
-
-    if (e.key === 37 /* Left */ || e.key === 39 /* Right */) {
-        player.setDrawing(1 /* Idle */);
-    }
-};
-
-player.addEventListener('left', moveLeft);
-player.addEventListener('right', moveRight);
-player.addEventListener('up', moveJump);
+});
 
 player.addEventListener('mousedown', function (e) {
     var button = "";
@@ -248,26 +238,17 @@ player.addEventListener('mousedown', function (e) {
     alert("Player clicked with " + button + " button!");
 });
 
-player.addEventListener('keyup', resetToIdle);
-
-player.addEventListener('touchstart', function (e) {
-    alert("player touched!");
-});
-
-// touch movement
-var touchGoRight = false;
-game.addEventListener('touchstart', function () {
-    touchGoRight = true;
-});
-game.addEventListener('touchend', function () {
-    touchGoRight = false;
-
-    moveJump();
-
+player.addEventListener('keyup', function (e) {
     if (inAir)
         return;
 
-    player.setDrawing(1 /* Idle */);
+    if (e.key === 37 /* Left */ || e.key === 39 /* Right */) {
+        player.setDrawing(1 /* Idle */);
+    }
+});
+
+player.addEventListener('touchstart', function () {
+    alert("player touched!");
 });
 
 var newScene = new ex.Scene();
@@ -357,10 +338,6 @@ player.addEventListener('update', function (data) {
         data.target.ay = 0;
     }
 
-    if (touchGoRight) {
-        moveRight();
-    }
-
     // Reset values because we don't know until we check the next update
     // inAir = true;
     isColliding = false;
@@ -417,11 +394,16 @@ emitter.emitRate = 494;
 emitter.opacity = 0.84;
 emitter.fadeFlag = true;
 emitter.particleLife = 2465;
-emitter.maxSize = 10;
-emitter.minSize = 1;
+emitter.maxSize = 1.5;
+emitter.minSize = .1;
 emitter.acceleration = new ex.Vector(0, 460);
 emitter.beginColor = ex.Color.Red;
 emitter.endColor = ex.Color.Yellow;
+emitter.particleSprite = blockSprite.clone();
+emitter.particleSprite.transformAboutPoint(new ex.Point(.5, .5));
+emitter.particleRotationalVelocity = Math.PI / 10;
+emitter.randomRotation = true;
+emitter.particleSprite.addEffect(new ex.Effects.Grayscale());
 
 //emitter.acceleration = new ex.Vector(0, -400);
 //emitter.particleSprite = spriteTiles.getSprite(0);
