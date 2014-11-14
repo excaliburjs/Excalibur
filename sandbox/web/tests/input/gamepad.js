42fb48ff2be225d4155f31a5b31ffa1b1e8c62fb
Kamran Ayub <kamran.ayub@gmail.com>
2014-11-13 20:59:52 -0600

    Split out gamepad sample for more detailed testing

---
+++ sandbox/web/tests/input/gamepad.js
@@ -0,0 +1,20 @@
+/// <reference path="../../../../dist/Excalibur.d.ts"/>
+var game = new ex.Engine(500, 500, "game");
+var padTexture = new ex.Texture("gamepad.png");
+game.start(new ex.Loader([padTexture])).then(start);
+function start() {
+    // Load gamepad sprite
+    var padSprite = new ex.Sprite(padTexture, 0, 0, padTexture.width, padTexture.height);
+    // Enable Gamepad support
+    game.input.gamepads.enabled = true;
+    // Draw gamepad
+    var gamepad = new ex.Actor(0, 0, padSprite.width, padSprite.height);
+    gamepad.addDrawing("bg", padSprite);
+    game.add(gamepad);
+    // todo add other buttons/actors
+    // Update global state on engine update
+    game.on("update", function (ue) {
+        document.getElementById("gamepad-num").innerHTML = game.input.gamepads.count().toString();
+    });
+}
+//# sourceMappingURL=gamepad.js.map
---
+++ sandbox/web/tests/input/gamepad.ts
@@ -0,0 +1,28 @@
+﻿/// <reference path="../../../../dist/Excalibur.d.ts"/>
+
+var game = new ex.Engine(500, 500, "game");
+var padTexture = new ex.Texture("gamepad.png");
+
+game.start(new ex.Loader([padTexture])).then(start);
+
+function start() {
+   // Load gamepad sprite
+   var padSprite = new ex.Sprite(padTexture, 0, 0, padTexture.width, padTexture.height);
+
+   // Enable Gamepad support
+   game.input.gamepads.enabled = true;
+   
+   // Draw gamepad
+   var gamepad = new ex.Actor(0, 0, padSprite.width, padSprite.height);
+   gamepad.addDrawing("bg", padSprite);
+   game.add(gamepad);
+
+   // todo add other buttons/actors
+   
+   // Update global state on engine update
+   game.on("update", (ue: ex.UpdateEvent) => {
+
+      document.getElementById("gamepad-num").innerHTML = game.input.gamepads.count().toString();
+
+   });
+}
