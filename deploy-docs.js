b358da400231b821230b94e21e7c51629cda13c9
Kamran Ayub <kamran.ayub@gmail.com>
2015-03-29 21:33:20 +0200

    Fix deploy docs script

--- deploy-docs.js
+++ deploy-docs.js
@@ -1,5 +1,29 @@
- var travisPing = require('travis-ping');
+var Travis = require('travis-ci');
+var repo = "excaliburjs/excaliburjs.github.io";
+var travis = new Travis({
+	version: '2.0.0'
+});
 
-travisPing.ping('kamranayub', process.env.GH_TOKEN, 'excaliburjs/excaliburjs.github.io', function(travisResponse) {
-	console.log(travisResponse);
+travis.authenticate({
+	github_token: process.env.GH_TOKEN
+}, function (err, res) {
+	if (err) {
+	  return console.error(err);
+	}
+
+	travis.repos(repo.split('/')[0], repo.split('/')[1]).builds.get(
+	  function (err, res) {
+		  if (err) {
+		    return console.error(err);
+		  }
+
+		  travis.requests.post({
+		    build_id: res.builds[0].id
+		  }, function (err, res) {
+		  	if (err) {
+		      return console.error(err);
+		    }
+		  	console.log(res.flash[0].notice);
+		  });
+	});
 });
