c542ab1cb21dcde0a3031e1bcbd7c5bf8e2bddbf
Kamran Ayub <kamran.ayub@gmail.com>
2015-03-30 17:02:49 +0200

    Done with Class

--- src/engine/Class.ts
+++ src/engine/Class.ts
@@ -1,17 +1,18 @@
 /// <reference path="Events.ts" />
 
 module ex {
+
    /**
-    * Excalibur base class
-    * @class Class
-    * @constructor 
+    * Excalibur base class that provides basic functionality such as [[EventDispatcher]]
+    * and extending abilities for vanilla Javascript projects
     */
    export class Class {
+
       /**
        * Direct access to the game object event dispatcher.
-       * @property eventDispatcher {EventDispatcher}
        */
       public eventDispatcher: EventDispatcher;
+
       constructor(){
          this.eventDispatcher = new EventDispatcher(this);
       }
@@ -19,9 +20,9 @@ module ex {
       /**
        * Add an event listener. You can listen for a variety of
        * events off of the engine; see the events section below for a complete list.
-       * @method addEventListener
-       * @param eventName {string} Name of the event to listen for
-       * @param handler {event=>void} Event handler for the thrown event
+       * @param eventName  Name of the event to listen for
+       * @param handler    Event handler for the thrown event
+       * @obsolete Use [[Class.on]] instead
        */
       public addEventListener(eventName: string, handler: (event?: GameEvent) => void) {
          this.eventDispatcher.subscribe(eventName, handler);
@@ -32,60 +33,68 @@ module ex {
        * it will remove all handlers registered for that specific event. If the eventName
        * and the handler instance are specified just that handler will be removed.
        *
-       * @method removeEventListener
-       * @param eventName {string} Name of the event to listen for
-       * @param [handler=undefined] {event=>void} Event handler for the thrown event
+       * @param eventName  Name of the event to listen for
+       * @param handler    Event handler for the thrown event
+       * @obsolete Use [[Class.off]] instead
        */
       public removeEventListener(eventName: string, handler?:(event?: GameEvent)=> void){
          this.eventDispatcher.unsubscribe(eventName, handler);
       }
 
       /**
-       * Alias for "addEventListener". You can listen for a variety of
+       * Alias for `addEventListener`. You can listen for a variety of
        * events off of the engine; see the events section below for a complete list.
-       * @method on
-       * @param eventName {string} Name of the event to listen for
-       * @param handler {event=>void} Event handler for the thrown event
+       * @param eventName  Name of the event to listen for
+       * @param handler    Event handler for the thrown event
        */
       public on(eventName: string, handler: (event?: GameEvent) => void) {
          this.eventDispatcher.subscribe(eventName, handler);
       }
+
       /**
-       * Alias for "removeEventListener". If only the eventName is specified
+       * Alias for `removeEventListener`. If only the eventName is specified
        * it will remove all handlers registered for that specific event. If the eventName
        * and the handler instance are specified only that handler will be removed.
        *
-       * @method off
-       * @param eventName {string} Name of the event to listen for
-       * @param [handler=undefined] {event=>void} Event handler for the thrown event
+       * @param eventName  Name of the event to listen for
+       * @param handler    Event handler for the thrown event
        */
       public off(eventName: string, handler?:(event?: GameEvent)=> void){
          this.eventDispatcher.unsubscribe(eventName, handler);
       }
 
       /**
-       * You may wish to extend native Excalibur functionality. Any method on 
-       * actor may be extended to support additional functionaliy. In the 
-       * example below we create a new type called "MyActor"
-       * <br/><b>Example</b><pre>var MyActor = Actor.extend({
-   constructor : function(){ 
-      this.newprop = 'something';
-      Actor.apply(this, arguments);
-   },
-   update : function(engine, delta){
-      // Implement custom update 
-
-         // Call super constructor update
-         Actor.prototype.update.call(this, engine, delta);
-         console.log("Something cool!");
-   }
-});
-var myActor = new MyActor(100, 100, 100, 100, Color.Azure);</pre>
-       * @method extend
-       * @static
-       * @param methods {any}
+       * You may wish to extend native Excalibur functionality in vanilla Javascript. 
+       * Any method on a class inheriting [[Class]] may be extended to support 
+       * additional functionaliy. In the example below we create a new type called `MyActor`.
+       * 
+       *
+       * ```js
+       * var MyActor = Actor.extend({
+       *
+       *    constructor: function() { 
+       *       this.newprop = 'something';
+       *       Actor.apply(this, arguments);
+       *    },
+       *
+       *    update: function(engine, delta) {
+       *       // Implement custom update 
+       *       // Call super constructor update
+       *       Actor.prototype.update.call(this, engine, delta);
+       *
+       *       console.log("Something cool!");
+       *    }
+       * });
+       * 
+       * var myActor = new MyActor(100, 100, 100, 100, Color.Azure);
+       * ```
+       *
+       * In TypeScript, you only need to use the `extends` syntax, you do not need
+       * to use this method of extension.
+       *
+       * @param methods A JSON object contain any methods/properties you want to extend
        */
-      public static extend(methods: any): any{
+      public static extend(methods: any): any {
           var parent: any = this;
           var child: any;
 
