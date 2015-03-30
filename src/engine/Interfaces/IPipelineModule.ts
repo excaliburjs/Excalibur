2bab40fe85383c70637c9d27f4ef55416a664bd3
Kamran Ayub <kamran.ayub@gmail.com>
2015-03-31 00:32:13 +0200

    Done with Interfaces

--- src/engine/Interfaces/IPipelineModule.ts
+++ src/engine/Interfaces/IPipelineModule.ts
@@ -1,7 +1,6 @@
 module ex {
     /**
     * An interface describing actor update pipeline modules
-    * @class ILoadable
     */
    export interface IPipelineModule {
       update(actor: Actor, engine: Engine, delta: number): void;
