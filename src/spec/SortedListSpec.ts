1f4022d935c39d698aa1681f533c8a4af9371dd7
Josh Edeen <josh.edeen@gmail.com>
2015-04-11 15:02:28 -0500

    SortedList tests

---
+++ src/spec/SortedListSpec.ts
@@ -0,0 +1,64 @@
+﻿/// <reference path="jasmine.d.ts" />
+/// <reference path="require.d.ts" />
+/// <reference path="../engine/Engine.ts" />
+/// <reference path="../engine/Util/SortedList.ts" />
+
+describe('A SortedList', () => {
+
+   var sortedList;
+
+   it('should be loaded', () => {
+      expect(ex.SortedList).toBeTruthy();
+   });
+
+   it('can have an element added to it at a non-existant node', () => {
+      //TODO
+      expect(true).toBe(false);
+   });
+
+   it('can have an element added to it at an existing node', () => {
+      //TODO
+      expect(true).toBe(false);
+   });
+
+   it('will indicate when an element is not present', () => {
+      //TODO
+      expect(true).toBe(false);
+   });
+
+   it('can have an element removed from it', () => {
+      //TODO
+      expect(true).toBe(false);
+   });
+
+   it('will not remove a node when removing an element, if that node contains other elements', () => {
+      //TODO
+      expect(true).toBe(false);
+   });
+
+   it('will remove a node when removing an element, when that node is empty', () => {
+      //TODO
+      expect(true).toBe(false);
+   });
+
+   it('will maintain the tree when removing an intermediate node', () => {
+      //TODO
+      expect(true).toBe(false);
+   });
+
+   it('will update its internal list every time an element is added or removed', () => {
+      //TODO
+      expect(true).toBe(false);
+   });
+
+   it('can return an ordered list of the elements it contains', () => {
+      //TODO
+      expect(true).toBe(false);
+   });
+
+   it('can return all of the elements at a specified key value', () => {
+      //TODO
+      expect(true).toBe(false);
+   });
+
+});
