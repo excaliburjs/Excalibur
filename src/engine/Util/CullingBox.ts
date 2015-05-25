275ac91c21db8d949fe8179dc351dc2d194a17ef
Josh Edeen <josh.edeen@gmail.com>
2015-05-25 17:11:27 -0500

    added bounding points to sprite for rotation cases

--- src/engine/Util/CullingBox.ts
+++ src/engine/Util/CullingBox.ts
@@ -7,7 +7,12 @@ module ex {
       private _bottomLeft: Point = new Point(0, 0);
       private _bottomRight: Point = new Point(0, 0);
 
-      private _screenCoords: Array<Point>;
+      private _xCoords: Array<number>;
+      private _yCoords: Array<number>;
+      private _xMin: number;
+      private _yMin: number;
+      private _xMax: number;
+      private _yMax: number;
 
       public isSpriteOffScreen(actor: Actor, engine: Engine): boolean {
          var drawingWidth = actor.currentDrawing.width * actor.currentDrawing.scale.x;
@@ -33,20 +38,29 @@ module ex {
 
          ///
 
-         this._screenCoords = new Array<Point>();
-
          var topLeftScreen = engine.worldToScreenCoordinates(this._topLeft);
          var topRightScreen = engine.worldToScreenCoordinates(this._topRight);
          var bottomLeftScreen = engine.worldToScreenCoordinates(this._bottomLeft);
          var bottomRightScreen = engine.worldToScreenCoordinates(this._bottomRight);
+         this._xCoords = [];
+         this._yCoords = [];
 
-         this._screenCoords.push(topLeftScreen, topRightScreen, bottomLeftScreen, bottomRightScreen);
+         this._xCoords.push(topLeftScreen.x, topRightScreen.x, bottomLeftScreen.x, bottomRightScreen.x);
+         this._yCoords.push(topLeftScreen.y, topRightScreen.y, bottomLeftScreen.y, bottomRightScreen.y);
 
-         for (var i = 0; i < this._screenCoords.length; i++) {
-            if (this._screenCoords[i].x > 0 &&
-               this._screenCoords[i].y > 0 &&
-               this._screenCoords[i].x < engine.width &&
-               this._screenCoords[i].y < engine.height) {
+         this._xMin = Math.min.apply(null, this._xCoords);
+         this._yMin = Math.min.apply(null, this._yCoords);
+         this._xMax = Math.max.apply(null, this._xCoords);
+         this._yMax = Math.max.apply(null, this._yCoords);
+
+         var boundingPoints = new Array<Point>();
+         boundingPoints.push(new Point(this._xMin, this._yMin), new Point(this._xMax, this._yMin), new Point(this._xMin, this._yMax), new Point(this._xMax, this._yMax));
+
+         for (var i = 0; i < boundingPoints.length; i++) {
+            if (boundingPoints[i].x > 0 &&
+               boundingPoints[i].y > 0 &&
+               boundingPoints[i].x < engine.width &&
+               boundingPoints[i].y < engine.height) {
                return false;
             }
          }
@@ -54,6 +68,13 @@ module ex {
       }
 
       public debugDraw(ctx: CanvasRenderingContext2D) {
+
+         // bounding rectangle
+         ctx.beginPath();
+         ctx.strokeStyle = Color.White.toString();
+         ctx.rect(this._xMin, this._yMin, this._xMax - this._xMin, this._yMax - this._yMin);
+         ctx.stroke();
+
          ctx.fillStyle = Color.Red.toString();
          ctx.beginPath();
          ctx.arc(this._topLeft.x, this._topLeft.y, 5, 0, Math.PI * 2);
