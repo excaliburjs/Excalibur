c48ffd6a58860f67fb89b48ad16d30ed14bf52a6
Josh Edeen <josh.edeen@gmail.com>
2015-05-31 16:34:08 -0500

    renamed pipeline, module folder

--- src/engine/UIActor.ts
+++ src/engine/UIActor.ts
@@ -17,9 +17,9 @@ module ex {
        */
       constructor(x?: number, y?: number, width?: number, height?: number) {
          super(x, y, width, height);
-         this.pipeline = [];
-         this.pipeline.push(new ex.MovementModule());
-         this.pipeline.push(new ex.CapturePointerModule());
+         this.traits = [];
+         this.traits.push(new ex.MovementModule());
+         this.traits.push(new ex.CapturePointerModule());
          this.anchor.setTo(0, 0);
          this.collisionType = ex.CollisionType.PreventCollision;
          this.enableCapturePointer = true;
