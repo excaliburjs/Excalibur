01a5e8c396cc82c557a47e57f6817111b64282f7
Kamran Ayub <kamran.ayub@gmail.com>
2015-10-24 17:22:20 -0500

    Trim rev-parse command output

--- deploy-docs.js
+++ deploy-docs.js
@@ -5,32 +5,34 @@ var travis = new Travis({
 	version: '2.0.0'
 });
 
-var branch = child_process.execSync("git rev-parse --abbrev-ref HEAD");
+var branch = child_process.execSync("git rev-parse --abbrev-ref HEAD").trim();
 
 if (branch !== "master") {
    process.stdout.write("Current branch is `" + branch + "`, skipping docs deployment...");
+} else {
+   process.stdout.write("Current branch is `" + branch + "`, triggering remote build of edge docs...");
+   
+   travis.authenticate({
+      github_token: process.env.GH_TOKEN
+   }, function (err, res) {
+      if (err) {
+      return console.error(err);
+      }
+   
+      travis.repos(repo.split('/')[0], repo.split('/')[1]).builds.get(
+      function (err, res) {
+         if (err) {
+            return console.error(err);
+         }
+   
+         travis.requests.post({
+            build_id: res.builds[0].id
+         }, function (err, res) {
+            if (err) {
+               return console.error(err);
+            }
+            console.log(res.flash[0].notice);
+         });
+      });
+   });
 }
-
-travis.authenticate({
-	github_token: process.env.GH_TOKEN
-}, function (err, res) {
-	if (err) {
-	  return console.error(err);
-	}
-
-	travis.repos(repo.split('/')[0], repo.split('/')[1]).builds.get(
-	  function (err, res) {
-		  if (err) {
-		    return console.error(err);
-		  }
-
-		  travis.requests.post({
-		    build_id: res.builds[0].id
-		  }, function (err, res) {
-		  	if (err) {
-		      return console.error(err);
-		    }
-		  	console.log(res.flash[0].notice);
-		  });
-	});
-});
