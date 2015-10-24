fbb364147e903f63d8604866faaf1e5fe846cfa1
Kamran Ayub <kamran.ayub@gmail.com>
2015-10-24 17:18:24 -0500

    Try to only build docs with master branch

--- deploy-docs.js
+++ deploy-docs.js
@@ -1,8 +1,15 @@
 var Travis = require('travis-ci');
+var child_process = require('child_process');
 var repo = "excaliburjs/excaliburjs.github.io";
 var travis = new Travis({
 	version: '2.0.0'
 });
+
+var branch = child_process.execSync("git rev-parse --abbrev-ref HEAD");
+
+if (branch !== "master") {
+   process.stdout.write("Current branch is `" + branch + "`, skipping docs deployment...");
+}
 
 travis.authenticate({
 	github_token: process.env.GH_TOKEN
