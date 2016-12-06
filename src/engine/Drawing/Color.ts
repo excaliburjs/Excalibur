be327193bb62a16c941b426dadc1a8560cda53ac
Kamran Ayub <kamran.ayub@gmail.com>
2016-12-05 21:29:23 -0600

    Fix compiler errors for 2.0

--- src/engine/Drawing/Color.ts
+++ src/engine/Drawing/Color.ts
@@ -318,6 +318,15 @@ module ex {
    class HSLColor {
       constructor(public h: number, public s: number, public l: number, public a: number) { }
 
+      public static hue2rgb(p: number, q: number, t: number): number {
+         if (t < 0) { t += 1; }
+         if (t > 1) { t -= 1; }
+         if (t < 1 / 6) { return p + (q - p) * 6 * t; }
+         if (t < 1 / 2) { return q; }
+         if (t < 2 / 3) { return p + (q - p) * (2 / 3 - t) * 6; }
+         return p;
+      }
+
       public static fromRGBA(r: number, g: number, b: number, a: number): HSLColor {
          r /= 255;
          g /= 255;
@@ -347,20 +356,12 @@ module ex {
          if (this.s === 0) {
             r = g = b = this.l; // achromatic
          } else {
-            function hue2rgb(p: number, q: number, t: number): number {
-               if (t < 0) { t += 1; }
-               if (t > 1) { t -= 1; }
-               if (t < 1 / 6) { return p + (q - p) * 6 * t; }
-               if (t < 1 / 2) { return q; }
-               if (t < 2 / 3) { return p + (q - p) * (2 / 3 - t) * 6; }
-               return p;
-            }
-
+            
             var q = this.l < 0.5 ? this.l * (1 + this.s) : this.l + this.s - this.l * this.s;
             var p = 2 * this.l - q;
-            r = hue2rgb(p, q, this.h + 1 / 3);
-            g = hue2rgb(p, q, this.h);
-            b = hue2rgb(p, q, this.h - 1 / 3);
+            r = HSLColor.hue2rgb(p, q, this.h + 1 / 3);
+            g = HSLColor.hue2rgb(p, q, this.h);
+            b = HSLColor.hue2rgb(p, q, this.h - 1 / 3);
          }
 
          return new Color(r * 255, g * 255, b * 255, this.a);
