26cd9a85d515668ce596219d293f2b2856125cdb
Garth Henson <garth@guahanweb.com>
2016-12-06 21:04:38 -0800

    Adding in report for browser feature support
    Adding debug logging to list out the supported browser features at startup time
    
    Removing preliminary attempt, which was unnecessarily heavy-handed
    
    cleanup

--- src/engine/Util/Detector.ts
+++ src/engine/Util/Detector.ts
@@ -5,9 +5,117 @@ module ex {
     * Excalibur internal feature detection helper class
     */
    export class Detector {
-      
+
+      private _features: Object = null;
+
       public failedTests: string[] = [];
+
+      public constructor() {
+         this._features = this._loadBrowserFeatures();
+      }
+
+      /**
+       * Returns a map of currently supported browser features. This method
+       * treats the features as a singleton and will only calculate feature
+       * support if it has not previously been done.
+       * 
+       * @return {Object} the list of supported features
+       */
+      public getBrowserFeatures(): Object {
+         if (this._features === null) {
+            this._features = this._loadBrowserFeatures();
+         }
+         return this._features;
+      }
       
+      /**
+       * Report on non-critical browser support for debugging purposes.
+       * Use native browser console colors for visibility.
+       * 
+       * @return {void}
+       */
+      public logBrowserFeatures(): void {
+         let features = {
+            webgl: 'WebGL',
+            webaudio: 'WebAudio',
+            gamepadapi: 'Gamepad API'
+         };
+
+         let msg = '%cSUPPORTED BROWSER FEATURES\n==========================%c\n';
+         let args = [
+            'font-weight: bold; color: navy',
+            'font-weight: normal; color: inherit'
+         ];
+
+         let supported = this.getBrowserFeatures();
+         for (let feature of Object.keys(features)) {
+            if (supported[feature]) {
+               msg += '(%c\u2713%c)';
+               args.push('font-weight: bold; color: green');
+               args.push('font-weight: normal; color: inherit');
+            } else {
+               msg += '(%c\u2717%c)'
+               args.push('font-weight: bold; color: red');
+               args.push('font-weight: normal; color: inherit'); 
+            };
+
+            msg += ' ' + features[feature] + '\n';
+         }         
+
+         args.unshift(msg);
+         console.log.apply(console, args);
+      }
+
+      /**
+       * Executes several IIFE's to get a constant reference to supported
+       * features within the current execution context.
+       * 
+       * @return {Object} a map of features
+       */
+      private _loadBrowserFeatures(): Object {
+         return {
+            // IIFE to check canvas support
+            canvas: (() => {
+               return this._criticalTests.canvasSupport();
+            })(),
+
+            // IIFE to check arraybuffer support
+            arraybuffer: (() => {
+               return this._criticalTests.arrayBufferSupport();
+            })(),
+
+            // IIFE to check dataurl support
+            dataurl: (() => {
+               return this._criticalTests.dataUrlSupport();
+            })(),
+
+            // IIFE to check objecturl support
+            objecturl: (() => {
+               return this._criticalTests.objectUrlSupport();
+            })(),
+
+            // IIFE to check rgba support
+            rgba: (() => {
+               return this._criticalTests.rgbaSupport();
+            })(),
+
+            // IIFE to check webaudio support
+            webaudio: (() => {
+               return this._warningTest.webAudioSupport();
+            })(),
+
+            // IIFE to check webgl support
+            webgl: (() => {
+               return this._warningTest.webglSupport();
+            })(),
+
+            // IIFE to check gamepadapi support
+            gamepadapi: (() => {
+               return !!(<any>navigator).getGamepads;
+            })()
+         };
+      }
+
       // critical browser features required for ex to run
       private _criticalTests = {
          // Test canvas/2d context support
@@ -61,7 +169,7 @@ module ex {
             return !!(elem.getContext && elem.getContext('webgl'));
          }
       };
-      
+
       public test(): boolean {			
          // Critical test will for ex not to run
          var failedCritical = false;
