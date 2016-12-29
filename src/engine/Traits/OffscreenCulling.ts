8194c068f788ca202956f5ec3a51edc0dff307d6
Kamran Ayub <kamran.ayub@gmail.com>
2016-12-29 13:58:05 -0600

    Convert Traits to modules

--- src/engine/Traits/OffscreenCulling.ts
+++ src/engine/Traits/OffscreenCulling.ts
@@ -1,52 +1,52 @@
-/// <reference path="../Interfaces/IActorTrait.ts" />
-/// <reference path="../Util/CullingBox.ts" />
-/// <reference path="../Actor.ts" />
+import { IActorTrait } from '../Interfaces/IActorTrait';
+import { Actor } from '../Actor';
+import { Engine } from '../Engine';
+import { Vector } from '../Algebra';
+import { ExitViewPortEvent, EnterViewPortEvent } from '../Events';
 
-module ex.Traits {
-   export class OffscreenCulling implements IActorTrait {
+export class OffscreenCulling implements IActorTrait {
 
-      public cullingBox: ex.CullingBox = new ex.CullingBox();
+   public cullingBox: ex.CullingBox = new ex.CullingBox();
 
-      public update(actor: Actor, engine: Engine, delta: number) {
-         var eventDispatcher = actor.eventDispatcher;
-         var anchor = actor.anchor;
-         var globalScale = actor.getGlobalScale();
-         var width = globalScale.x * actor.getWidth() / actor.scale.x;
-         var height = globalScale.y * actor.getHeight() / actor.scale.y;
-         var worldPos = actor.getWorldPos();
-         var actorScreenCoords = engine.worldToScreenCoordinates(new Vector(worldPos.x - anchor.x * width, 
-                                                                            worldPos.y - anchor.y * height));
+   public update(actor: Actor, engine: Engine, delta: number) {
+      var eventDispatcher = actor.eventDispatcher;
+      var anchor = actor.anchor;
+      var globalScale = actor.getGlobalScale();
+      var width = globalScale.x * actor.getWidth() / actor.scale.x;
+      var height = globalScale.y * actor.getHeight() / actor.scale.y;
+      var worldPos = actor.getWorldPos();
+      var actorScreenCoords = engine.worldToScreenCoordinates(new Vector(worldPos.x - anchor.x * width, 
+                                                                           worldPos.y - anchor.y * height));
 
-         var zoom = 1.0;
-         if (actor.scene && actor.scene.camera) {
-            zoom = Math.abs(actor.scene.camera.getZoom());   
+      var zoom = 1.0;
+      if (actor.scene && actor.scene.camera) {
+         zoom = Math.abs(actor.scene.camera.getZoom());   
+      }
+      
+      var isSpriteOffScreen = true;
+      if (actor.currentDrawing != null) {
+         isSpriteOffScreen = this.cullingBox.isSpriteOffScreen(actor, engine);
+      }
+      
+      if (!actor.isOffScreen) {
+         if ((actorScreenCoords.x + width * zoom < 0 || 
+            actorScreenCoords.y + height * zoom < 0 ||
+            actorScreenCoords.x > engine.width ||
+            actorScreenCoords.y > engine.height) &&
+            isSpriteOffScreen ) {
+            
+            eventDispatcher.emit('exitviewport', new ExitViewPortEvent());
+            actor.isOffScreen = true;
          }
-         
-         var isSpriteOffScreen = true;
-         if (actor.currentDrawing != null) {
-            isSpriteOffScreen = this.cullingBox.isSpriteOffScreen(actor, engine);
-         }
-         
-         if (!actor.isOffScreen) {
-            if ((actorScreenCoords.x + width * zoom < 0 || 
-               actorScreenCoords.y + height * zoom < 0 ||
-               actorScreenCoords.x > engine.width ||
-               actorScreenCoords.y > engine.height) &&
-               isSpriteOffScreen ) {
-               
-               eventDispatcher.emit('exitviewport', new ExitViewPortEvent());
-               actor.isOffScreen = true;
-            }
-         } else {
-            if ((actorScreenCoords.x + width * zoom > 0 &&
-               actorScreenCoords.y + height * zoom > 0 &&
-               actorScreenCoords.x < engine.width &&
-               actorScreenCoords.y < engine.height) ||
-               !isSpriteOffScreen) {
-               
-               eventDispatcher.emit('enterviewport', new EnterViewPortEvent());               
-               actor.isOffScreen = false;
-            }
+      } else {
+         if ((actorScreenCoords.x + width * zoom > 0 &&
+            actorScreenCoords.y + height * zoom > 0 &&
+            actorScreenCoords.x < engine.width &&
+            actorScreenCoords.y < engine.height) ||
+            !isSpriteOffScreen) {
+            
+            eventDispatcher.emit('enterviewport', new EnterViewPortEvent());               
+            actor.isOffScreen = false;
          }
       }
    }
--- src/engine/Traits/TileMapCollisionDetection.ts
+++ src/engine/Traits/TileMapCollisionDetection.ts
@@ -1,41 +1,44 @@
-/// <reference path="../Interfaces/IActorTrait.ts" />
+import { IActorTrait } from '../Interfaces/IActorTrait';
+import { Actor, CollisionType } from '../Actor';
+import { Engine } from '../Engine';
+import { Vector } from '../Algebra';
+import { Side } from '../Collision/Side';
+import { CollisionEvent } from '../Events';
 
-module ex.Traits {
-   export class TileMapCollisionDetection implements IActorTrait { 
-      public update(actor: Actor, engine: Engine, delta: number) {
-         var eventDispatcher = actor.eventDispatcher;
-         if (actor.collisionType !== CollisionType.PreventCollision && engine.currentScene && engine.currentScene.tileMaps) {            
+export class TileMapCollisionDetection implements IActorTrait { 
+   public update(actor: Actor, engine: Engine, delta: number) {
+      var eventDispatcher = actor.eventDispatcher;
+      if (actor.collisionType !== CollisionType.PreventCollision && engine.currentScene && engine.currentScene.tileMaps) {            
 
-            for (var j = 0; j < engine.currentScene.tileMaps.length; j++) {
-               var map = engine.currentScene.tileMaps[j];
-               var intersectMap: Vector;
-               var side = Side.None;
-               var max = 2;
-               var hasBounced = false;
-               while (intersectMap = map.collides(actor)) {
-                  if (max-- < 0) {
-                     break;
-                  } 
-                  side = actor.getSideFromIntersect(intersectMap);
-                  eventDispatcher.emit('collision', new CollisionEvent(actor, null, side, intersectMap));
-                  if ((actor.collisionType === CollisionType.Active || actor.collisionType === CollisionType.Elastic)) {
-                     actor.pos.y += intersectMap.y;
-                     actor.pos.x += intersectMap.x;
+         for (var j = 0; j < engine.currentScene.tileMaps.length; j++) {
+            var map = engine.currentScene.tileMaps[j];
+            var intersectMap: Vector;
+            var side = Side.None;
+            var max = 2;
+            var hasBounced = false;
+            while (intersectMap = map.collides(actor)) {
+               if (max-- < 0) {
+                  break;
+               } 
+               side = actor.getSideFromIntersect(intersectMap);
+               eventDispatcher.emit('collision', new CollisionEvent(actor, null, side, intersectMap));
+               if ((actor.collisionType === CollisionType.Active || actor.collisionType === CollisionType.Elastic)) {
+                  actor.pos.y += intersectMap.y;
+                  actor.pos.x += intersectMap.x;
 
-                     // Naive elastic bounce
-                     if (actor.collisionType === CollisionType.Elastic && !hasBounced) {
-                        hasBounced = true;
-                        if (side === Side.Left) {
-                           actor.vel.x = Math.abs(actor.vel.x);
-                        } else if (side === Side.Right) {
-                           actor.vel.x = -Math.abs(actor.vel.x);
-                        } else if (side === Side.Top) {
-                           actor.vel.y = Math.abs(actor.vel.y);
-                        } else if (side === Side.Bottom) {
-                           actor.vel.y = -Math.abs(actor.vel.y);
-                        }
-                     }                 
-                  }
+                  // Naive elastic bounce
+                  if (actor.collisionType === CollisionType.Elastic && !hasBounced) {
+                     hasBounced = true;
+                     if (side === Side.Left) {
+                        actor.vel.x = Math.abs(actor.vel.x);
+                     } else if (side === Side.Right) {
+                        actor.vel.x = -Math.abs(actor.vel.x);
+                     } else if (side === Side.Top) {
+                        actor.vel.y = Math.abs(actor.vel.y);
+                     } else if (side === Side.Bottom) {
+                        actor.vel.y = -Math.abs(actor.vel.y);
+                     }
+                  }                 
                }
             }
          }
