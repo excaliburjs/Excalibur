b2396b4bfdb75cdea554d9493d61d9db0d8b2a58
Kamran Ayub <kamran.ayub@gmail.com>
2016-12-30 09:48:54 -0600

    Convert TileMap, Timer, Trigger, UIActor

--- src/engine/UIActor.ts
+++ src/engine/UIActor.ts
@@ -1,39 +1,39 @@
-/// <reference path="Actor.ts" />
+import { Vector } from './Algebra';
+import { Engine } from './Engine';
+import { Actor, CollisionType } from './Actor';
+import * as Traits from './Traits/Index';
 
-module ex {   
+/**
+ * Helper [[Actor]] primitive for drawing UI's, optimized for UI drawing. Does
+ * not participate in collisions. Drawn on top of all other actors.
+ */
+export class UIActor extends Actor {
+   protected _engine: Engine;
 
    /**
-    * Helper [[Actor]] primitive for drawing UI's, optimized for UI drawing. Does
-    * not participate in collisions. Drawn on top of all other actors.
-    */     
-   export class UIActor extends Actor {
-      protected _engine: Engine;
+    * @param x       The starting x coordinate of the actor
+    * @param y       The starting y coordinate of the actor
+    * @param width   The starting width of the actor
+    * @param height  The starting height of the actor
+    */
+   constructor(x?: number, y?: number, width?: number, height?: number) {
+      super(x, y, width, height);
+      this.traits = [];
+      this.traits.push(new Traits.EulerMovement());
+      this.traits.push(new Traits.CapturePointer());
+      this.anchor.setTo(0, 0);
+      this.collisionType = CollisionType.PreventCollision;
+      this.enableCapturePointer = true;
+   }
 
-      /**
-       * @param x       The starting x coordinate of the actor
-       * @param y       The starting y coordinate of the actor
-       * @param width   The starting width of the actor
-       * @param height  The starting height of the actor
-       */
-      constructor(x?: number, y?: number, width?: number, height?: number) {
-         super(x, y, width, height);
-         this.traits = [];
-         this.traits.push(new Traits.EulerMovement());
-         this.traits.push(new Traits.CapturePointer());
-         this.anchor.setTo(0, 0);
-         this.collisionType = ex.CollisionType.PreventCollision;
-         this.enableCapturePointer = true;
-      }
+   public onInitialize(engine: Engine) {
+      this._engine = engine;
+   }
 
-      public onInitialize(engine: Engine) {
-         this._engine = engine;
-      }
+   public contains(x: number, y: number, useWorld: boolean = true) {
+      if (useWorld) { return super.contains(x, y); }
 
-      public contains(x: number, y: number, useWorld: boolean = true) {
-         if (useWorld) { return super.contains(x, y); }
-
-         var coords = this._engine.worldToScreenCoordinates(new ex.Vector(x, y));
-         return super.contains(coords.x, coords.y);
-      }
+      var coords = this._engine.worldToScreenCoordinates(new Vector(x, y));
+      return super.contains(coords.x, coords.y);
    }
 }
