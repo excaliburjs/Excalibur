3cf00f912413fd8ffeff9a7969e36e422bf213a7
Kamran Ayub <kamran.ayub@gmail.com>
2016-12-30 22:54:02 -0600

    Exported public API

---
+++ src/engine/Collision/Index.ts
@@ -0,0 +1,6 @@
+export * from './Body';
+export * from './BoundingBox';
+export * from './CircleArea';
+export * from './EdgeArea';
+export * from './PolygonArea';
+export * from './Side';
--- src/engine/Index.ts
+++ src/engine/Index.ts
@@ -2,3 +2,49 @@
 export * from './Actor';
 export * from './Algebra';
 export * from './Camera';
+export * from './Class';
+export * from './Debug';
+export * from './Engine';
+export * from './EventDispatcher';
+export * from './Events';
+export * from './Group';
+export * from './Label';
+export * from './Loader';
+export * from './Particles';
+export * from './Physics';
+export * from './Promises';
+export * from './Scene';
+export * from './TileMap';
+export * from './Timer';
+export * from './Trigger';
+export * from './UIActor';
+
+export * from './Actions/Index';
+export * from './Collision/Index';
+export * from './Drawing/Index';
+
+// ex.Events namespace
+import * as events from './Events';
+export { events as Events }
+
+// ex.Input namespace
+import * as input from './Input/Index';
+export { input as Input }
+
+export * from './PostProcessing/Index';
+export * from './Resources/Index';
+
+// ex.Traits namespace
+import * as traits from './Traits/Index';
+export { traits as Traits }
+
+// ex.Util and ex.Util.DrawUtil namespaces
+import * as util from './Util/Util';
+import * as drawUtil from './Util/DrawUtil';
+(<any>util).DrawUtil = drawUtil;
+
+export { util as Util }
+
+export * from './Util/CullingBox';
+export * from './Util/EasingFunctions';
+export * from './Util/Log';
