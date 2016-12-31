be81af5abc78719d9a5ed976436c594dd551ffdc
Kamran Ayub <kamran.ayub@gmail.com>
2016-12-31 01:35:28 -0600

    Use UMD module wrapper for dist

--- src/browser/start.js
+++ src/browser/start.js
@@ -1,13 +1,16 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
-        //Allow using this built library as an AMD module
-        //in another project. That other project will only
-        //see this AMD call, not the internal modules in
-        //the closure below.
-        define([], factory);
+        // AMD. Register as an anonymous module.
+        define([], function (b) {
+            return (root.ex = factory(b));
+        });
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
     } else {
-        //Browser globals case. Just assign the
-        //result to a property on the global.
+        // Browser globals
         root.ex = factory();
     }
 }(this, function () {
