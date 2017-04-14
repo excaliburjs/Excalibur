a2a78b6ab550a52b9e6f47f28dc45f18ed7119bc
Kamran Ayub <kamran.ayub@gmail.com>
2017-04-13 22:21:57 -0500

    Add typedoc build step

--- GruntFile.js
+++ GruntFile.js
@@ -1,7 +1,12 @@
 /*********************************
 /* Excalibur.js Grunt Build File
 /*********************************/
-var path = require('path');
+const path = require('path');
+const fs = require('fs');
+const child_process = require('child_process');
+const rimraf = require('rimraf');
+const TYPEDOC_CMD = path.join('node_modules', '.bin', 'typedoc');
+
 var appveyorBuild = process.env.APPVEYOR_BUILD_NUMBER || '';
 
 if (appveyorBuild) {
@@ -17,7 +22,7 @@ module.exports = function (grunt) {
    grunt.initConfig({
       pkg: grunt.file.readJSON('package.json'),
       version: '<%= pkg.version %>' + appveyorBuild,
-      tscCmd: path.join('node_modules', '.bin', 'tsc'),
+
       //
       // Clean dists and tests
       //
@@ -341,5 +346,43 @@ module.exports = function (grunt) {
    grunt.registerTask('appveyor', ['default', 'shell:nuget']);
 
    // CI task to deploy dists
-   grunt.registerTask('dists', ['buildcontrol']);   
+   grunt.registerTask('dists', ['bumpup', 'buildcontrol']);  
+
+   // CI task to build API docs
+   // Typically called by excaliburjs.github.io CI job to generate versioned docs
+   grunt.registerTask('apidocs', 'Build API documentation', function (version) {
+      version = version || "Edge";
+
+      console.log("Removing existing docs...");
+
+      rimraf.sync('docs/api/');
+
+      console.log("Compiling default template (default)...");
+      try {
+         if (!fs.existsSync('./typedoc-default-themes/node_modules')) {
+            child_process.execSync("npm install", { cwd: "./typedoc-default-themes", stdio: [0,1,2] });
+         }
+      } catch (e) {
+         // fails to execute Linux commands, OK
+      }
+
+      console.log("Executing typedoc...");
+
+      child_process.execSync(TYPEDOC_CMD + 
+         ' --name "Excalibur.js ' + version + ' API Documentation"' +
+         ' --mode modules' +
+         ' --readme src/engine/Docs/Index.md' +         
+         ' --includes src/engine/Docs' +
+         ' --out docs/api' +
+         ' --theme typedoc-default-themes/bin/default' +
+         ' --hideGenerator' +
+         ' --excludePrivate' +
+         ' --listInvalidSymbolLinks' +
+         ' --gaID UA-46390208-1' +
+         ' --gaSite excaliburjs.com' +
+         ' --tsconfig src/engine/tsconfig.json' +
+         ' src/engine', {
+         stdio: [0,1,2]
+      });
+   });
 };
