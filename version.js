9f973f69b0c57c992488b99ab709a5fb1dcaf53a
Kamran Ayub <kamran.ayub@gmail.com>
2017-04-17 20:23:15 -0500

    Use sync-request instead for synchronous version getting

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-const request       = require('request');
+const request       = require('sync-request');
 const appveyorBuild = process.env.APPVEYOR_BUILD_NUMBER || '';
 const travisBuild   = process.env.TRAVIS_BUILD_NUMBER   || '';
 const commit        = process.env.TRAVIS_COMMIT         || '';
@@ -16,32 +16,30 @@ if (!process.env.GH_TOKEN) {
 }
 
 // Fetch latest GH release tag version
-request({
-   uri: 'https://api.github.com/repos/excaliburjs/Excalibur/releases/latest',
+var res = request('GET', 'https://api.github.com/repos/excaliburjs/Excalibur/releases/latest', {
    headers: { 
       'User-Agent': 'excaliburjs/0.1',
       'Authorization': 'token ' + process.env.GH_TOKEN
    }
-}, function (error, res, body) {
-   const statusCode = res.statusCode;
-
-   if (statusCode !== 200) {
-      throw Error('Fatal error fetching GH release version, status: ' + statusCode + ', ' + error);
-   }
-
-   const tag_name = JSON.parse(body).tag_name;
-   const version  = tag_name.match(/^v?([0-9\.]+)$/)[1]; // strip v prefix
- 
-   // Nuget doesn't yet support the + suffix in versions
-   const appveyVersion = version + '.' + appveyorBuild + '-alpha';   
-   const travisVersion = version + '-alpha.' + travisBuild + '+' + commit.substring(0, 7);
-
-   if (appveyorBuild) {
-      console.info('Using Appveyor build as version', appveyVersion);
-      module.exports = appveyVersion;
-   } else {
-      console.info('Using Travis build as version', travisVersion);
-      module.exports = travisVersion;
-   }
-
 });
+
+const statusCode = res.statusCode;
+
+if (statusCode !== 200) {
+   throw Error('Fatal error fetching GH release version, status: ' + statusCode);
+}
+
+const tag_name = JSON.parse(body).tag_name;
+const version  = tag_name.match(/^v?([0-9\.]+)$/)[1]; // strip v prefix
+
+// Nuget doesn't yet support the + suffix in versions
+const appveyVersion = version + '.' + appveyorBuild + '-alpha';   
+const travisVersion = version + '-alpha.' + travisBuild + '+' + commit.substring(0, 7);
+
+if (appveyorBuild) {
+   console.info('Using Appveyor build as version', appveyVersion);
+   module.exports = appveyVersion;
+} else {
+   console.info('Using Travis build as version', travisVersion);
+   module.exports = travisVersion;
+}
