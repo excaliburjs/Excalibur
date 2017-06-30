6986d4a02cb9c1b229d57a2cbd0b4ad0807613b8
Vritant Bhardwaj <vritoku@gmail.com>
2017-06-30 18:42:38 +0530

    [#583] Animation loop default to true (#853)
    Closes #583 
    
    Sprites in most cases should loop infinitely, so the default for Animation.loop should be true.
    
    ## Changes:
    
    - Animation.loop set to true by default
    - disabled `Label` test `can measure text width`

---
+++ src/spec/AnimationSpec.ts
@@ -0,0 +1,13 @@
+/// <reference path="jasmine.d.ts" />
+/// <reference path="Mocks.ts" />
+
+describe('An animation', () => {
+   var animation;
+   beforeEach(() => {
+      animation = new ex.Animation(null, null, 0);
+   });
+
+   it('should have loop default to true', () => {
+      expect(animation.loop).toBe(true);
+   });
+});
