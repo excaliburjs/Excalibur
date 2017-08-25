420c707457869695875fc96b710a4b28061aaf4b
alanag13 <alan.grgic@gmail.com>
2017-08-24 20:43:56 -0500

    allow for proper overriding of default values

--- src/engine/Configurable.ts
+++ src/engine/Configurable.ts
@@ -1,22 +1,38 @@
 export type Constructor<T> = {
-   new(...args: any[]): T
+   new(...args: any[]): T;
 }
 
-export function Configurable<T extends Constructor<Object>>(base: T) : T {
+export interface IDefaultable<T> {
+   getDefaultPropVals(): Partial<T>; 
+}
+
+export function Configurable<T extends Constructor<IDefaultable<T>>>(base: T) : T {
       return class extends base {
-         public setProperties(props: Partial<T>): this {                 
+         public assign(props: Partial<T>): this {
+               
+               //set the value of every property that was passed in,
+               //if the constructor previously set this value, it will be overridden here
                for (var k in props) {
                   if (typeof (<any>this)[k] !== 'function') {
                      (<any>this)[k] = (<any>props)[k];
                   }
                }
+
+               //set default property values only if they WEREN'T set
+               //by either the passed in properties or the constructor
+               for (var i in this.getDefaultPropVals()){
+                  if (typeof (<any>this)[i] === 'undefined'){
+                     (<any>this)[i] = (<any>props)[i];
+                  }
+               }
+
                return this;
             }
 
             constructor(...args: any[]) {
                super(...args);
-               if (args.length === 1 && typeof args[0] === {}) {
-                     this.setProperties(args[0]);
+               if (args.length === 1 && typeof args[0] === 'object') {
+                     this.assign(args[0]);
                }
             }
 
