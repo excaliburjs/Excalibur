f6edcfd84506279747baeef061325ef8c4fc5d6f
eonarheim <erik.onarheim@gmail.com>
2017-09-21 19:09:52 -0500

    Actually fix coverage

--- GruntFile.js
+++ GruntFile.js
@@ -284,7 +284,7 @@ module.exports = function (grunt) {
       //
       coveralls: {
          main: {
-            src: './coverage/lcov/**/lcov.info',
+            src: './coverage/lcov.info',
             options: {
                force: true
             }
