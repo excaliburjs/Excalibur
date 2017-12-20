4208a8f381aa3a75b658fa0967fceb4e5bb042d2
Alan Grgic <alan.grgic@gmail.com>
2017-12-20 13:28:28 -0600

    use setTargetAtTime to set volume for Sounds where available

--- src/engine/Resources/Sound.ts
+++ src/engine/Resources/Sound.ts
@@ -567,7 +567,11 @@ class WebAudioInstance implements IAudio {
 
    public setVolume(value: number) {
       this._volume = value;
-      this._volumeNode.gain.value = Util.clamp(value, 0, 1.0);
+      if (this._volumeNode.gain.setTargetAtTime) {
+         this._volumeNode.gain.setTargetAtTime(Util.clamp(value, 0, 1.0), audioContext.currentTime, 0);
+      } else {
+         this._volumeNode.gain.value = Util.clamp(value, 0, 1.0);
+      }
    }
 
    public setLoop(value: boolean) {
