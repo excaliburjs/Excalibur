7535d083e949d71239fd7cdbbecef624b88981d5
Erik Onarheim <erik.onarheim@gmail.com>
2017-12-22 19:07:34 -0600

    Update Sound.ts

--- src/engine/Resources/Sound.ts
+++ src/engine/Resources/Sound.ts
@@ -563,7 +563,6 @@ class WebAudioInstance implements IAudio {
    }
 
    public setVolume(value: number) {
-      this._volume = value;
       if (this._volumeNode.gain.setTargetAtTime) {
          this._volumeNode.gain.setTargetAtTime(Util.clamp(value, 0, 1.0), audioContext.currentTime, 0);
       } else {
