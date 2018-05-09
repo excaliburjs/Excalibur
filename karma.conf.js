9f917ebd974c4d32edccbb5d4d57901a85bdb663
Erik Onarheim <erik.onarheim@gmail.com>
2018-05-09 10:01:05 -0500

    Fix code coverage error, switch to excat dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function (config) {
       // list of files / patterns to load in the browser
       files: [
          './build/dist/excalibur.js',
+         './build/dist/excalibur.js.map',
          './build/dist/index.d.ts',
          './src/engine/**/*.ts',
          './src/spec/images/**/*.png',
@@ -66,7 +67,7 @@ module.exports = function (config) {
       // test results reporter to use
       // possible values: 'dots', 'progress'
       // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-      reporters: ['progress', 'karma-typescript', 'coverage'],
+      reporters: ['progress', 'coverage'],
 
       coverageReporter: {
          reporters: [
