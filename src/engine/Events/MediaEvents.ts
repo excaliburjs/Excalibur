33c3786518710d4a5b06d19ab36e1b2e94d2b375
Roman <r.dyro@itransition.com>
2018-05-18 17:03:28 +0300

    Create separate scope for Events. Create MediaEvent Class && NativeSoundEvent Class

---
+++ src/engine/Events/MediaEvents.ts
@@ -0,0 +1,60 @@
+import { GameEvent } from '../Events';
+import { Sound } from '../Resources/Sound/Sound';
+import { Actor } from '../Actor';
+
+export class MediaEvent extends GameEvent<Sound> {
+   /**
+    * Media event cannot bubble
+    */
+   public get bubbles(): boolean {
+      return false;
+   };
+   /**
+    * Media event cannot bubble, so they have no path
+    */
+   protected get _path(): Actor[] {
+      return null;
+   };
+
+
+   constructor(public target: Sound, protected _name: string = 'MediaEvent') {
+      super();
+   }
+
+   /**
+    * Prevents event from bubbling
+    */
+   public stopPropagation(): void {
+      /**
+       * Stub
+       */
+   }
+   /**
+    * Action, that calls when event happens
+    */
+   public action(): void {
+      /**
+       * Stub
+       */
+   }
+   /**
+    * Propagate event further through event path
+    */
+   public propagate(): void {
+      /**
+       * Stub
+       */
+   }
+
+   public layPath(_actor: Actor): void {
+      /**
+       * Stub
+       */
+   }
+}
+
+export class NativeSoundEvent extends MediaEvent {
+   constructor(target: Sound) {
+      super(target, 'NativeSoundEvent');
+   }
+}
