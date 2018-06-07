9cd14b2105fc92515d259720cb6465880662ade4
Erik Onarheim <erik.onarheim@gmail.com>
2018-06-07 07:21:54 -0500

    Fix Offscreen culling regression

--- src/engine/Traits/OffscreenCulling.ts
+++ src/engine/Traits/OffscreenCulling.ts
@@ -17,6 +17,7 @@ export class OffscreenCulling implements IActorTrait {
     var cameraPos = engine.currentScene.camera.pos;
     var worldPos = actor.getWorldPos();
     var actorScreenCoords = engine.worldToScreenCoordinates(new Vector(worldPos.x - anchor.x * width, worldPos.y - anchor.y * height));
+    var cameraScreenCoords = engine.worldToScreenCoordinates(cameraPos);
 
     var zoom = 1.0;
     if (actor.scene && actor.scene.camera) {
@@ -32,8 +33,8 @@ export class OffscreenCulling implements IActorTrait {
       if (
         (actorScreenCoords.x + width * zoom < 0 ||
           actorScreenCoords.y + height * zoom < 0 ||
-          actorScreenCoords.x > engine.halfDrawWidth + cameraPos.x ||
-          actorScreenCoords.y > engine.halfDrawHeight + cameraPos.y) &&
+          actorScreenCoords.x > engine.halfDrawWidth + cameraScreenCoords.x ||
+          actorScreenCoords.y > engine.halfDrawHeight + cameraScreenCoords.y) &&
         isSpriteOffScreen
       ) {
         eventDispatcher.emit('exitviewport', new ExitViewPortEvent(actor));
@@ -43,8 +44,8 @@ export class OffscreenCulling implements IActorTrait {
       if (
         (actorScreenCoords.x + width * zoom > 0 &&
           actorScreenCoords.y + height * zoom > 0 &&
-          actorScreenCoords.x < engine.halfDrawWidth + cameraPos.x &&
-          actorScreenCoords.y < engine.halfDrawHeight + cameraPos.y) ||
+          actorScreenCoords.x < engine.halfDrawWidth + cameraScreenCoords.x &&
+          actorScreenCoords.y < engine.halfDrawHeight + cameraScreenCoords.y) ||
         !isSpriteOffScreen
       ) {
         eventDispatcher.emit('enterviewport', new EnterViewPortEvent(actor));
