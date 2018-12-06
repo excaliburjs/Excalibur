a5b368acc5ceeb3d582812b7000785a83c995a18
Kevin Cantrell <kevin903@live.com>
2018-12-06 10:33:36 -0500

    transparency working with excalibur colors

--- sandbox/tests/gif/gif.ts
+++ sandbox/tests/gif/gif.ts
@@ -6,12 +6,10 @@ var game = new ex.Engine({
   height: 400
 });
 
-var gif: ex.Gif = new ex.Gif('https://raw.githubusercontent.com/kevin192291/Excalibur/master/sandbox/tests/gif/sword.gif', true);
+var gif: ex.Gif = new ex.Gif('sword.gif', ex.Color.Black);
 var loader = new ex.Loader([gif]);
 game.start(loader).then(() => {
-  var spriteSheet = gif.asSpriteSheet();
-  var playerIdleAnimation = spriteSheet.getAnimationForAll(game, 125);
   var actor = new ex.Actor(100, 100, 500, 500);
-  actor.addDrawing('idle', playerIdleAnimation);
+  actor.addDrawing('idle', gif.asAnimation(game, 500));
   game.add(actor);
 });
