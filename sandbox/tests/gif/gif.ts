1312a562234ea580d9b00113a5ca57a0cb80c4d1
Kevin Cantrell <kevin903@live.com>
2018-12-12 23:33:54 -0500

    changed swing name

--- sandbox/tests/gif/gif.ts
+++ sandbox/tests/gif/gif.ts
@@ -10,6 +10,6 @@ var gif: ex.Gif = new ex.Gif('sword.gif', ex.Color.Black);
 var loader = new ex.Loader([gif]);
 game.start(loader).then(() => {
   var actor = new ex.Actor(game.canvasWidth / 2, game.canvasHeight / 2, gif.width, gif.height);
-  actor.addDrawing('idle', gif.asAnimation(game, 500));
+  actor.addDrawing('swing', gif.asAnimation(game, 500));
   game.add(actor);
 });
