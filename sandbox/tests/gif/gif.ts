508b38cfebe322d482b0871ec8218c0af6459272
Kevin Cantrell <kevin903@live.com>
2018-12-12 23:34:56 -0500

    added dot

--- sandbox/tests/gif/gif.ts
+++ sandbox/tests/gif/gif.ts
@@ -6,7 +6,7 @@ var game = new ex.Engine({
   height: 400
 });
 
-var gif: ex.Gif = new ex.Gif('sword.gif', ex.Color.Black);
+var gif: ex.Gif = new ex.Gif('./sword.gif', ex.Color.Black);
 var loader = new ex.Loader([gif]);
 game.start(loader).then(() => {
   var actor = new ex.Actor(game.canvasWidth / 2, game.canvasHeight / 2, gif.width, gif.height);
