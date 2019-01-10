360dbe0a510b8c4401d0ed84c025c743f14525b0
eonarheim <erik.onarheim@gmail.com>
2019-01-09 18:41:24 -0600

    Perhaps multi matches causes problems

--- src/spec/GifSpec.ts
+++ src/spec/GifSpec.ts
@@ -68,10 +68,6 @@ describe('A Gif', () => {
       const frame1 = animation.sprites[0];
       const frame2 = animation.sprites[1];
 
-      frame1.draw(engine.ctx, 0, 0);
-      imagediff.expectCanvasImageMatches('GifSpec/frame1.png', engine.canvas, done);
-
-      engine.ctx.clearRect(0, 0, engine.canvas.width, engine.canvas.height);
       frame2.draw(engine.ctx, 0, 0);
       imagediff.expectCanvasImageMatches('GifSpec/frame2.png', engine.canvas, done);
     });
