64d48e8a3b1d8f012a5be2595bb90f032394e2a4
eonarheim <erik.onarheim@gmail.com>
2019-04-19 15:34:07 -0500

    WIP CollisionGroups

---
+++ src/engine/Collision/CollisionGroup.ts
@@ -0,0 +1,41 @@
+/**
+ * CollisionGroups indicate like members that do not collide with each other. Use [[CollisionGroupManager]] to create [[CollisionGroups]]
+ *
+ * For example:
+ *
+ * Players have collision group A
+ * Enemies have collision group B
+ * Blocks have collision group C
+ *
+ * Players don't collide with each other, but enemies and blocks. Likewise, enemies don't collide with each other but collide
+ * with players and blocks.
+ */
+export class CollisionGroup {
+  public static All = new CollisionGroup('Collide with all', -1, -1);
+
+  private _name: string;
+  private _category: number;
+  private _mask: number;
+
+  constructor(name: string, category: number, mask: number) {
+    this._name = name;
+    this._category = category;
+    this._mask = mask;
+  }
+
+  public get name() {
+    return this._name;
+  }
+
+  public get category() {
+    return this._category;
+  }
+
+  public get mask() {
+    return this._mask;
+  }
+
+  public shouldCollide(other: CollisionGroup): boolean {
+    return (this.category & other.mask) !== 0 && (other.category & this.mask) !== 0;
+  }
+}
---
+++ src/engine/Collision/CollisionGroupManager.ts
@@ -0,0 +1,33 @@
+import { CollisionGroup } from './CollisionGroup';
+
+export class CollisionGroupManager {
+  // using bitmasking the maximum number of groups is 32, because that is the heighest 32bit integer that JS can present.
+  private static _MAX_GROUPS = 32;
+  private static _currentGroup = 1;
+  private static _currentBit = 0b1 | 0;
+  private static _groups: { [name: string]: CollisionGroup } = {};
+
+  /**
+   * Create a new named collision group
+   * @param name Name for the collision group
+   * @param mask Optionally provide your own 32-bit mask, if none is provide the manager will generate one
+   */
+  public static create(name: string, mask?: number) {
+    if (this._currentGroup > this._MAX_GROUPS) {
+      throw new Error('Cannot have more than 32 collision groups');
+    }
+    const group = new CollisionGroup(name, this._currentBit, mask !== undefined ? mask : ~this._currentBit);
+    this._currentBit = (this._currentBit << 1) | 0;
+    this._currentGroup++;
+    this._groups[name] = group;
+    return group;
+  }
+
+  public static get groups(): CollisionGroup[] {
+    return Object.keys(this._groups).map((g) => this._groups[g]);
+  }
+
+  public static groupByName(name: string) {
+    return this._groups[name];
+  }
+}
