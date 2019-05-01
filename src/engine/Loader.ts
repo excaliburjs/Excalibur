cc350a7d09d39faff7542950fe61508244626842
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-05-01 15:18:56 +0100

    Loader.ts: Prefer const/let to var

--- src/engine/Loader.ts
+++ src/engine/Loader.ts
@@ -138,7 +138,7 @@ export class Loader extends Class implements CanLoad {
    * Return a html button element for excalibur to use as a play button
    */
   public startButtonFactory = () => {
-    let buttonElement = document.createElement('button');
+    const buttonElement = document.createElement('button');
     buttonElement.id = 'excalibur-play';
     buttonElement.textContent = this.playButtonText;
     buttonElement.style.display = 'none';
@@ -165,7 +165,7 @@ export class Loader extends Class implements CanLoad {
    * @param loadable  Resource to add
    */
   public addResource(loadable: Loadable) {
-    var key = this._index++;
+    const key = this._index++;
     this._resourceList.push(loadable);
     this._progressCounts[key] = 0;
     this._totalCounts[key] = 1;
@@ -177,8 +177,8 @@ export class Loader extends Class implements CanLoad {
    * @param loadables  The list of resources to load
    */
   public addResources(loadables: Loadable[]) {
-    var i = 0,
-      len = loadables.length;
+    let i = 0;
+    const len = loadables.length;
 
     for (i; i < len; i++) {
       this.addResource(loadables[i]);
@@ -201,7 +201,7 @@ export class Loader extends Class implements CanLoad {
     } else {
       this._playButtonShown = true;
       this._playButton.style.display = 'block';
-      let promise = new Promise();
+      const promise = new Promise();
 
       this._playButton.addEventListener('click', () => (promise.state() === PromiseState.Pending ? promise.resolve() : promise));
       this._playButton.addEventListener('touchend', () => (promise.state() === PromiseState.Pending ? promise.resolve() : promise));
@@ -221,8 +221,8 @@ export class Loader extends Class implements CanLoad {
    * that resolves when loading of all is complete
    */
   public load(): Promise<any> {
-    var complete = new Promise<any>();
-    var me = this;
+    const complete = new Promise<any>();
+    const me = this;
     if (this._resourceList.length === 0) {
       me.showPlayButton().then(() => {
         // Unlock audio context in chrome after user gesture
@@ -237,19 +237,19 @@ export class Loader extends Class implements CanLoad {
       return complete;
     }
 
-    var progressArray = new Array<any>(this._resourceList.length);
-    var progressChunks = this._resourceList.length;
+    const progressArray = new Array<any>(this._resourceList.length);
+    const progressChunks = this._resourceList.length;
 
     this._resourceList.forEach((r, i) => {
       if (this._engine) {
         r.wireEngine(this._engine);
       }
       r.onprogress = function(e) {
-        var total = <number>e.total;
-        var loaded = <number>e.loaded;
+        const total = <number>e.total;
+        const loaded = <number>e.loaded;
         progressArray[i] = { loaded: (loaded / total) * (100 / progressChunks), total: 100 };
 
-        var progressResult: any = progressArray.reduce(
+        const progressResult: any = progressArray.reduce(
           function(accum, next) {
             return { loaded: accum.loaded + next.loaded, total: 100 };
           },
@@ -296,14 +296,14 @@ export class Loader extends Class implements CanLoad {
    * to customize the drawing, or just override entire method.
    */
   public draw(ctx: CanvasRenderingContext2D) {
-    let canvasHeight = this._engine.canvasHeight / this._engine.pixelRatio;
-    let canvasWidth = this._engine.canvasWidth / this._engine.pixelRatio;
+    const canvasHeight = this._engine.canvasHeight / this._engine.pixelRatio;
+    const canvasWidth = this._engine.canvasWidth / this._engine.pixelRatio;
 
     if (this._playButtonRootElement) {
-      let left = ctx.canvas.offsetLeft;
-      let top = ctx.canvas.offsetTop;
-      let buttonWidth = this._playButton.clientWidth;
-      let buttonHeight = this._playButton.clientHeight;
+      const left = ctx.canvas.offsetLeft;
+      const top = ctx.canvas.offsetTop;
+      const buttonWidth = this._playButton.clientWidth;
+      const buttonHeight = this._playButton.clientHeight;
       this._playButtonRootElement.style.left = `${left + canvasWidth / 2 - buttonWidth / 2}px`;
       this._playButtonRootElement.style.top = `${top + canvasHeight / 2 - buttonHeight / 2 + 100}px`;
     }
@@ -311,12 +311,12 @@ export class Loader extends Class implements CanLoad {
     ctx.fillStyle = this.backgroundColor;
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-    var y = canvasHeight / 2;
-    var width = Math.min(this.logoWidth, canvasWidth * 0.75);
-    var x = canvasWidth / 2 - width / 2;
+    const y = canvasHeight / 2;
+    const width = Math.min(this.logoWidth, canvasWidth * 0.75);
+    const x = canvasWidth / 2 - width / 2;
 
-    var imageHeight = Math.floor(width * (this.logoHeight / this.logoWidth)); // OG height/width factor
-    var oldAntialias = this._engine.getAntialiasing();
+    const imageHeight = Math.floor(width * (this.logoHeight / this.logoWidth)); // OG height/width factor
+    const oldAntialias = this._engine.getAntialiasing();
     this._engine.setAntialiasing(true);
     ctx.drawImage(this._image, 0, 0, this.logoWidth, this.logoHeight, x, y - imageHeight - 20, width, imageHeight);
 
@@ -328,10 +328,10 @@ export class Loader extends Class implements CanLoad {
 
     ctx.lineWidth = 2;
     DrawUtil.roundRect(ctx, x, y, width, 20, 10);
-    var progress = width * (this._numLoaded / this._resourceCount);
-    var margin = 5;
-    var progressWidth = progress - margin * 2;
-    var height = 20 - margin * 2;
+    const progress = width * (this._numLoaded / this._resourceCount);
+    const margin = 5;
+    const progressWidth = progress - margin * 2;
+    const height = 20 - margin * 2;
     DrawUtil.roundRect(ctx, x + margin, y + margin, progressWidth > 0 ? progressWidth : 0, height, 5, null, Color.White);
     this._engine.setAntialiasing(oldAntialias);
   }
