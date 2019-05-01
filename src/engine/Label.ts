f98dd1dcb2b4be939ba6c7c3ad2d1a6e86bec85f
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-05-01 15:09:46 +0100

    Label.ts: Prefer const/let to var

--- src/engine/Label.ts
+++ src/engine/Label.ts
@@ -235,9 +235,9 @@ export class LabelImpl extends Actor {
    * @param ctx  Rendering context to measure the string with
    */
   public getTextWidth(ctx: CanvasRenderingContext2D): number {
-    var oldFont = ctx.font;
+    const oldFont = ctx.font;
     ctx.font = this._fontString;
-    var width = ctx.measureText(this.text).width;
+    const width = ctx.measureText(this.text).width;
     ctx.font = oldFont;
     return width;
   }
@@ -297,7 +297,7 @@ export class LabelImpl extends Actor {
   }
 
   private _lookupFontStyle(fontStyle: FontStyle): string {
-    var boldstring = this.bold ? ' bold' : '';
+    const boldstring = this.bold ? ' bold' : '';
     switch (fontStyle) {
       case FontStyle.Italic:
         return 'italic' + boldstring;
@@ -369,8 +369,8 @@ export class LabelImpl extends Actor {
         opacity: this.opacity
       });
     } else {
-      var oldAlign = ctx.textAlign;
-      var oldTextBaseline = ctx.textBaseline;
+      const oldAlign = ctx.textAlign;
+      const oldTextBaseline = ctx.textBaseline;
 
       ctx.textAlign = this._lookupTextAlign(this.textAlign);
       ctx.textBaseline = this._lookupBaseAlign(this.baseAlign);
