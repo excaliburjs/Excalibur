217119f48e984fabc3f6da2e8c2b09511b6fca46
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-05-02 21:39:57 +0100

    RandomSpec.ts: Prefer const/let to var

--- src/spec/RandomSpec.ts
+++ src/spec/RandomSpec.ts
@@ -6,111 +6,111 @@ describe('A random number', () => {
   });
 
   it('can be constructed', () => {
-    var random = new ex.Random();
+    const random = new ex.Random();
     expect(random).not.toBeNull();
   });
 
   it('can be seeded to produce the same sequence', () => {
-    var random1 = new ex.Random(10);
-    var random2 = new ex.Random(10);
+    const random1 = new ex.Random(10);
+    const random2 = new ex.Random(10);
 
     expect(random1.seed).toBe(random2.seed);
-    for (var i = 0; i < 9000; i++) {
+    for (let i = 0; i < 9000; i++) {
       expect(random1.next()).toBe(random2.next());
     }
   });
 
   it('produces the correct first number for a specific seed according to original paper', () => {
     // C implemenation output http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/MT2002/emt19937ar.html
-    var random = new ex.Random(19650218);
+    const random = new ex.Random(19650218);
     expect(random.seed).toBe(19650218);
     expect(random.nextInt()).toBe(2325592414);
   });
 
   it('produces the correct 1000th number for a specific seed according to original paper', () => {
-    var random = new ex.Random(19650218);
+    const random = new ex.Random(19650218);
 
-    for (var i = 0; i < 999; i++) {
+    for (let i = 0; i < 999; i++) {
       random.nextInt();
     }
     expect(random.nextInt()).toBe(1746987133);
   });
 
   it('produces the correct 22000th number for a specific seed according to original paper', () => {
-    var random = new ex.Random(19650218);
+    const random = new ex.Random(19650218);
 
-    for (var i = 0; i < 21999; i++) {
+    for (let i = 0; i < 21999; i++) {
       random.nextInt();
     }
     expect(random.nextInt()).toBe(3203887892);
   });
 
   it('can be seeded to produce the different sequences', () => {
-    var random1 = new ex.Random(10);
-    var random2 = new ex.Random(50);
+    const random1 = new ex.Random(10);
+    const random2 = new ex.Random(50);
 
     expect(random1.seed).not.toBe(random2.seed);
-    for (var i = 0; i < 9000; i++) {
+    for (let i = 0; i < 9000; i++) {
       expect(random1.next()).not.toBe(random2.next());
     }
   });
 
   it('can be seeded to produce numbers between [0, 1)', () => {
-    var random1 = new ex.Random(10);
-    for (var i = 0; i < 9000; i++) {
-      var r = random1.next();
-      var inrange = 0 <= r && r < 1.0;
+    const random1 = new ex.Random(10);
+    for (let i = 0; i < 9000; i++) {
+      const r = random1.next();
+      const inrange = 0 <= r && r < 1.0;
       expect(inrange).toBe(true, `Random ${r} not in range [0, 1). Seed [${random1.seed}] Iteration [${i}]`);
     }
   });
 
   it('can be seeded to produce numbers in an arbitrary floating point range', () => {
-    var random1 = new ex.Random(10);
-    for (var i = 0; i < 9000; i++) {
-      var r = random1.floating(-88, 1900);
-      var inrange = -88 <= r && r < 1900;
+    const random1 = new ex.Random(10);
+    for (let i = 0; i < 9000; i++) {
+      const r = random1.floating(-88, 1900);
+      const inrange = -88 <= r && r < 1900;
       expect(inrange).toBe(true, `Random ${r} not in range [-88, 1900). Seed [${random1.seed}] Iteration [${i}]`);
     }
   });
 
   it('can be seeded to produce numbers in an arbitrary integer range', () => {
-    var random1 = new ex.Random(10);
-    for (var i = 0; i < 9000; i++) {
-      var r = random1.integer(-10, 10);
-      var inrange = -10 <= r && r <= 10;
+    const random1 = new ex.Random(10);
+    for (let i = 0; i < 9000; i++) {
+      const r = random1.integer(-10, 10);
+      const inrange = -10 <= r && r <= 10;
       expect(inrange).toBe(true, `Random ${r} not in range [-10, 10]. Seed [${random1.seed}] Iteration [${i}]`);
     }
   });
 
   it('can do coin flips', () => {
-    var random1 = new ex.Random(10);
-    var truthCount = 0;
-    var falseCount = 0;
+    const random1 = new ex.Random(10);
+    let truthCount = 0;
+    let falseCount = 0;
 
-    for (var i = 0; i < 9000; i++) {
-      var b = random1.bool();
+    for (let i = 0; i < 9000; i++) {
+      const b = random1.bool();
       if (b) {
         truthCount++;
       } else {
         falseCount++;
       }
     }
-    var ratio = truthCount / falseCount;
+    const ratio = truthCount / falseCount;
     expect(ratio).toBeCloseTo(1.0, 0.1, `Bool did not appear to be 50/50 Ratio true/false [${ratio}]`);
   });
 
   it('can pick an element out of an array', () => {
-    var array = ['one', 'two', 'three', 'four'];
+    const array = ['one', 'two', 'three', 'four'];
 
-    var random1 = new ex.Random(10);
+    const random1 = new ex.Random(10);
 
-    var one = 0;
-    var two = 0;
-    var three = 0;
-    var four = 0;
+    let one = 0;
+    let two = 0;
+    let three = 0;
+    let four = 0;
 
-    for (var i = 0; i < 1000; i++) {
-      let thing = random1.pickOne(array);
+    for (let i = 0; i < 1000; i++) {
+      const thing = random1.pickOne(array);
       switch (thing) {
         case 'one':
           one++;
@@ -134,13 +134,13 @@ describe('A random number', () => {
     expect(three).toBeGreaterThan(0);
     expect(four).toBeGreaterThan(0);
 
-    var ratio = one / two / (three / four);
+    const ratio = one / two / (three / four);
     expect(ratio).toBeCloseTo(1.0, 0.1, 'Should pick elements equally');
   });
 
   it('can pick a subset of an array', () => {
-    var random1 = new ex.Random(10);
-    var array = ['one', 'two', 'three', 'four'];
+    const random1 = new ex.Random(10);
+    const array = ['one', 'two', 'three', 'four'];
 
     expect(() => random1.pickSet(array, 5)).toThrowError('Invalid number of elements to pick, must pick a value 0 < n <= length');
     expect(() => random1.pickSet(array, -1)).toThrowError('Invalid number of elements to pick, must pick a value 0 < n <= length');
@@ -149,12 +149,12 @@ describe('A random number', () => {
   });
 
   it('can pick a set of an array with dups', () => {
-    var array = ['one', 'two', 'three', 'four'];
-    var numCounts = 1000;
+    const array = ['one', 'two', 'three', 'four'];
+    const numCounts = 1000;
 
-    var random1 = new ex.Random(10);
+    const random1 = new ex.Random(10);
 
-    var counts = {
+    const counts = {
       one: {
         count: 0,
         lastIndex: -1
@@ -173,7 +173,7 @@ describe('A random number', () => {
       }
     };
 
-    var newSet = random1.pickSet(array, numCounts, true);
+    const newSet = random1.pickSet(array, numCounts, true);
 
     for (let i = 0; i < numCounts; i++) {
       const element = counts[newSet[i]];
@@ -193,19 +193,19 @@ describe('A random number', () => {
     expect(counts.three.count).toBeGreaterThan(0);
     expect(counts.four.count).toBeGreaterThan(0);
 
-    var countsSum = counts.one.count + counts.two.count + counts.three.count + counts.four.count;
+    const countsSum = counts.one.count + counts.two.count + counts.three.count + counts.four.count;
     expect(countsSum).toEqual(numCounts);
 
-    var ratio = counts.one.count / counts.two.count / (counts.three.count / counts.four.count);
+    const ratio = counts.one.count / counts.two.count / (counts.three.count / counts.four.count);
     expect(ratio).toBeCloseTo(1.0, 0.1, 'Should pick elements equally');
   });
 
   it('can shuffle arrays', () => {
-    var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-    var random1 = new ex.Random(10);
+    const random1 = new ex.Random(10);
 
-    var shuffled = random1.shuffle(array);
+    const shuffled = random1.shuffle(array);
 
     expect(array.length).toBe(shuffled.length);
     // to be fair this won't necessarily be true all the time
@@ -214,15 +214,15 @@ describe('A random number', () => {
   });
 
   it('can generate a random range', () => {
-    var random1 = new ex.Random(10);
+    const random1 = new ex.Random(10);
 
-    var randomArray = random1.range(1000, 0, 4);
+    const randomArray = random1.range(1000, 0, 4);
 
     expect(randomArray.length).toBe(1000);
 
-    var expectedValue = (0 + 4) / 2;
+    const expectedValue = (0 + 4) / 2;
 
-    var average =
+    const average =
       randomArray.reduce((acc, curr, i, arr) => {
         return acc + curr;
       }, 0) / 1000;
@@ -231,12 +231,12 @@ describe('A random number', () => {
   });
 
   it('can do d4 dice rolls', () => {
-    var d4min = 900;
-    var d4max = -1;
+    let d4min = 900;
+    let d4max = -1;
 
-    var random1 = new ex.Random(10);
-    for (var i = 0; i < 2000; i++) {
-      let roll = random1.d4();
+    const random1 = new ex.Random(10);
+    for (let i = 0; i < 2000; i++) {
+      const roll = random1.d4();
       d4min = Math.min(roll, d4min);
       d4max = Math.max(roll, d4max);
     }
@@ -246,12 +246,12 @@ describe('A random number', () => {
   });
 
   it('can do d6 dice rolls', () => {
-    var d6min = 900;
-    var d6max = -1;
+    let d6min = 900;
+    let d6max = -1;
 
-    var random1 = new ex.Random(10);
-    for (var i = 0; i < 2000; i++) {
-      let roll = random1.d6();
+    const random1 = new ex.Random(10);
+    for (let i = 0; i < 2000; i++) {
+      const roll = random1.d6();
       d6min = Math.min(roll, d6min);
       d6max = Math.max(roll, d6max);
     }
@@ -261,12 +261,12 @@ describe('A random number', () => {
   });
 
   it('can do d8 dice rolls', () => {
-    var min = 900;
-    var max = -1;
+    let min = 900;
+    let max = -1;
 
-    var random1 = new ex.Random(10);
-    for (var i = 0; i < 2000; i++) {
-      let roll = random1.d8();
+    const random1 = new ex.Random(10);
+    for (let i = 0; i < 2000; i++) {
+      const roll = random1.d8();
       min = Math.min(roll, min);
       max = Math.max(roll, max);
     }
@@ -276,12 +276,12 @@ describe('A random number', () => {
   });
 
   it('can do d10 dice rolls', () => {
-    var min = 900;
-    var max = -1;
+    let min = 900;
+    let max = -1;
 
-    var random1 = new ex.Random(10);
-    for (var i = 0; i < 2000; i++) {
-      let roll = random1.d10();
+    const random1 = new ex.Random(10);
+    for (let i = 0; i < 2000; i++) {
+      const roll = random1.d10();
       min = Math.min(roll, min);
       max = Math.max(roll, max);
     }
@@ -291,12 +291,12 @@ describe('A random number', () => {
   });
 
   it('can do d12 dice rolls', () => {
-    var min = 900;
-    var max = -1;
+    let min = 900;
+    let max = -1;
 
-    var random1 = new ex.Random(10);
-    for (var i = 0; i < 2000; i++) {
-      let roll = random1.d12();
+    const random1 = new ex.Random(10);
+    for (let i = 0; i < 2000; i++) {
+      const roll = random1.d12();
       min = Math.min(roll, min);
       max = Math.max(roll, max);
     }
@@ -306,12 +306,12 @@ describe('A random number', () => {
   });
 
   it('can do d20 dice rolls', () => {
-    var min = 900;
-    var max = -1;
+    let min = 900;
+    let max = -1;
 
-    var random1 = new ex.Random(10);
-    for (var i = 0; i < 2000; i++) {
-      let roll = random1.d20();
+    const random1 = new ex.Random(10);
+    for (let i = 0; i < 2000; i++) {
+      const roll = random1.d20();
       min = Math.min(roll, min);
       max = Math.max(roll, max);
     }
