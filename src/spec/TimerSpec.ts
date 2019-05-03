a4dc9da1a6361e9218ddd68a77adfe8fb3782ae4
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-05-03 01:28:41 +0100

    TimerSpec.ts: Prefer const/let to var

--- src/spec/TimerSpec.ts
+++ src/spec/TimerSpec.ts
@@ -2,9 +2,9 @@ import * as ex from '../../build/dist/excalibur';
 import { TestUtils } from './util/TestUtils';
 
 describe('A Timer', () => {
-  var timer;
-  var scene: ex.Scene;
-  var engine: ex.Engine;
+  let timer;
+  let scene: ex.Scene;
+  let engine: ex.Engine;
 
   beforeEach(() => {
     engine = TestUtils.engine({
@@ -19,13 +19,13 @@ describe('A Timer', () => {
   });
 
   it('has a unique id', () => {
-    var newtimer = new ex.Timer(function() {
+    const newtimer = new ex.Timer(function() {
       /*do nothing*/
     }, 500);
     expect(timer.id).not.toBe(newtimer.id);
     expect(timer.id).toBe(newtimer.id - 1);
 
-    var newtimer2 = new ex.Timer(function() {
+    const newtimer2 = new ex.Timer(function() {
       /*do nothing*/
     }, 500);
     expect(timer.id).not.toBe(newtimer2.id);
@@ -42,7 +42,7 @@ describe('A Timer', () => {
 
   it('can repeat itself indefinitely at a specified interval', () => {
     // count the number of fires
-    var count = 0;
+    let count = 0;
     timer = new ex.Timer(
       function() {
         count++;
@@ -62,7 +62,7 @@ describe('A Timer', () => {
     // count the number of fires
     timer = new ex.Timer(
       function() {
-        var dummy = 0;
+        const dummy = 0;
       },
       500,
       true,
@@ -83,7 +83,7 @@ describe('A Timer', () => {
   });
 
   it('can be canceled', () => {
-    var count = 0;
+    let count = 0;
     timer = new ex.Timer(
       function() {
         count++;
@@ -120,7 +120,7 @@ describe('A Timer', () => {
 
   it('has no completed state when running forever', () => {
     // count the number of fires
-    var count = 0;
+    let count = 0;
     timer = new ex.Timer(
       function() {
         count++;
@@ -147,7 +147,7 @@ describe('A Timer', () => {
   });
 
   it('can be reset at the same interval', () => {
-    var count = 0;
+    let count = 0;
     // non-repeating timer
     timer = new ex.Timer(
       function() {
@@ -175,7 +175,7 @@ describe('A Timer', () => {
   });
 
   it('can be reset at a different interval', () => {
-    var count = 0;
+    let count = 0;
     // non-repeating timer
     timer = new ex.Timer(
       function() {
@@ -203,7 +203,7 @@ describe('A Timer', () => {
   });
 
   it('can be reset on a repeating timer', () => {
-    var count = 0;
+    let count = 0;
     // non-repeating timer
     timer = new ex.Timer(
       function() {
@@ -220,7 +220,7 @@ describe('A Timer', () => {
 
     timer.reset(30);
 
-    for (var i = 0; i < 100; i++) {
+    for (let i = 0; i < 100; i++) {
       scene.update(engine, 31);
       expect(count).toBe(2 + i);
       expect(timer.complete).toBe(false);
@@ -231,7 +231,7 @@ describe('A Timer', () => {
     // non-repeating timer
     timer = new ex.Timer(
       function() {
-        var dummy = 0;
+        const dummy = 0;
       },
       500,
       true,
@@ -261,9 +261,9 @@ describe('A Timer', () => {
   });
 
   it('can be paused', () => {
-    var count = 0;
+    let count = 0;
     // arrange
-    var timer = new ex.Timer(
+    const timer = new ex.Timer(
       () => {
         count++;
       },
@@ -282,9 +282,9 @@ describe('A Timer', () => {
   });
 
   it('can be unpaused', () => {
-    var count = 0;
+    let count = 0;
     // arrange
-    var timer = new ex.Timer(
+    const timer = new ex.Timer(
       () => {
         count++;
       },
