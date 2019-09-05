85e07953a3810a46a98798a358876cba62a53b6f
Kamran Ayub <kamran.ayub@gmail.com>
2019-09-04 23:53:14 -0500

    Use async/await

--- src/engine/Actions/Actions.stories.ts
+++ src/engine/Actions/Actions.stories.ts
@@ -9,23 +9,23 @@ storiesOf('Actions', module)
   .addDecorator(withKnobs as any)
   .add(
     'fade',
-    withEngine((game) => {
+    withEngine(async (game) => {
       const hrt = new Texture(heartTexture);
       const ldr = new Loader([hrt]);
 
-      game.start(ldr).then(() => {
-        const heart = new Actor(game.halfCanvasWidth, game.halfCanvasHeight, 50, 50);
-        heart.addDrawing(hrt);
+      await game.start(ldr);
 
-        heart.opacity = 0;
+      const heart = new Actor(game.halfCanvasWidth, game.halfCanvasHeight, 50, 50);
+      heart.addDrawing(hrt);
 
-        game.add(heart);
+      heart.opacity = 0;
 
-        heart.actions
-          .fade(number('Starting Opacity', 1), number('Duration', 200))
-          .delay(number('Delay', 2000))
-          .fade(number('Ending Opacity', 0), number('Duration', 200));
-      });
+      game.add(heart);
+
+      heart.actions
+        .fade(number('Starting Opacity', 1), number('Duration', 200))
+        .delay(number('Delay', 2000))
+        .fade(number('Ending Opacity', 0), number('Duration', 200));
     }),
     { notes: 'Should show a heart and fade out after 2 seconds' }
   );
