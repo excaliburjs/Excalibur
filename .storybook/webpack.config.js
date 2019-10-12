67f19bf9ba1f43e4007ea166ea254a0fda706b9c
Kamran Ayub <kamran.ayub@gmail.com>
2019-10-11 21:50:47 -0500

    Remove mdx loader

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,23 +1,6 @@
-const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin');
 const path = require('path');
 
 module.exports = ({ config }) => {
-  config.module.rules.push({
-    test: /\.(stories|story)\.mdx$/,
-    use: [
-      {
-        loader: 'babel-loader'
-        // may or may not need this line depending on your app's setup
-        // plugins: ['@babel/plugin-transform-react-jsx']
-      },
-      {
-        loader: '@mdx-js/loader',
-        options: {
-          compilers: [createCompiler({})]
-        }
-      }
-    ]
-  });
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     use: [
