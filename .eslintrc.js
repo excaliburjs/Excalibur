739f8c48f63d85882033642b8969a894d15efd97
Erik Onarheim <erik.onarheim@gmail.com>
2019-10-16 21:39:09 -0500

    Fix up eslint + vscode complaints

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: ['./src/tsconfig.json', './src/spec/_tsconfig.json'],
+    project: ['./src/tsconfig.json', './src/spec/tsconfig.json'],
     sourceType: 'module'
   },
   plugins: ['@typescript-eslint', '@typescript-eslint/tslint'],
