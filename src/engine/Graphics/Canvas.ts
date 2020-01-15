b18f1ae113531bc81aa8a1fd62f6824540cff34c
Erik Onarheim <erik.onarheim@gmail.com>
2020-01-14 18:21:02 -0600

    Canvas abstraction for custom drawing

--- src/engine/Graphics/Canvas.ts
+++ src/engine/Graphics/Canvas.ts
@@ -1,10 +1,11 @@
-import { Graphic, DrawOptions, GraphicOptions } from './Graphic';
+import { DrawOptions, GraphicOptions } from './Graphic';
+import { Raster } from './Raster';
 
 export interface CanvasOptions {
   drawHandler: (ctx: CanvasRenderingContext2D, delta: number) => void;
 }
 
-export class Canvas extends Graphic {
+export class Canvas extends Raster {
   constructor(public options: GraphicOptions & CanvasOptions) {
     super(options);
   }
