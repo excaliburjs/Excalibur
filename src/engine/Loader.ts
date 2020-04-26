75df8cd27347f9c31a8a6cc580e393496e2f24e9
Erik Onarheim <erik.onarheim@gmail.com>
2020-04-25 23:04:37 -0500

    First pass at updating loader
    * Add id to play button root element
    * Make positioning and colors flexible of all things on screen
    * Needs tests

--- src/engine/Loader.ts
+++ src/engine/Loader.ts
@@ -10,6 +10,7 @@ import * as DrawUtil from './Util/DrawUtil';
 
 import logoImg from './Loader.logo.png';
 import loaderCss from './Loader.css';
+import { Vector } from './Algebra';
 
 /**
  * Pre-loading assets
@@ -93,7 +94,31 @@ export class Loader extends Class implements CanLoad {
   public logo = logoImg;
   public logoWidth = 468;
   public logoHeight = 118;
-  public backgroundColor = '#176BAA';
+  /**
+   * Positions the top left corner of the logo image
+   * If not set, the loader automatically positions the logo
+   */
+  public logoPosition: Vector | null;
+  /**
+   * Positions the top left corner of the play button.
+   * If not set, the loader automatically positions the play button
+   */
+  public playButtonPosition: Vector | null;
+  /**
+   * Positions the top left corner of the loading bar
+   * If not set, the loader automatically positions the loading bar
+   */
+  public loadingBarPosition: Vector | null;
+
+  /**
+   * Gets or sets the color of the loading bar, default is [[Color.White]]
+   */
+  public loadingBarColor: Color = Color.White;
+
+  /**
+   * Gets or sets the background color of the loader as a hex string
+   */
+  public backgroundColor: string = '#176BAA';
 
   protected _imageElement: HTMLImageElement;
   protected get _image() {
@@ -114,6 +139,7 @@ export class Loader extends Class implements CanLoad {
   protected get _playButton() {
     if (!this._playButtonRootElement) {
       this._playButtonRootElement = document.createElement('div');
+      this._playButtonRootElement.id = 'excalibur-play-root';
       this._playButtonRootElement.style.position = 'absolute';
       document.body.appendChild(this._playButtonRootElement);
     }
@@ -244,13 +270,13 @@ export class Loader extends Class implements CanLoad {
       if (this._engine) {
         r.wireEngine(this._engine);
       }
-      r.onprogress = function(e) {
+      r.onprogress = function (e) {
         const total = <number>e.total;
         const loaded = <number>e.loaded;
         progressArray[i] = { loaded: (loaded / total) * (100 / progressChunks), total: 100 };
 
         const progressResult: any = progressArray.reduce(
-          function(accum, next) {
+          function (accum, next) {
             return { loaded: accum.loaded + next.loaded, total: 100 };
           },
           { loaded: 0, total: 100 }
@@ -258,7 +284,7 @@ export class Loader extends Class implements CanLoad {
 
         me.onprogress.call(me, progressResult);
       };
-      r.oncomplete = r.onerror = function() {
+      r.oncomplete = r.onerror = function () {
         me._numLoaded++;
         if (me._numLoaded === me._resourceCount) {
           setTimeout(() => {
@@ -281,7 +307,7 @@ export class Loader extends Class implements CanLoad {
       if (!list[index]) {
         return;
       }
-      list[index].load().then(function() {
+      list[index].load().then(function () {
         loadNext(list, index + 1);
       });
     }
@@ -304,21 +330,35 @@ export class Loader extends Class implements CanLoad {
       const top = ctx.canvas.offsetTop;
       const buttonWidth = this._playButton.clientWidth;
       const buttonHeight = this._playButton.clientHeight;
-      this._playButtonRootElement.style.left = `${left + canvasWidth / 2 - buttonWidth / 2}px`;
-      this._playButtonRootElement.style.top = `${top + canvasHeight / 2 - buttonHeight / 2 + 100}px`;
+      if (this.playButtonPosition) {
+        this._playButtonRootElement.style.left = `${this.playButtonPosition.x}px`;
+        this._playButtonRootElement.style.top = `${this.playButtonPosition.y}px`;
+      } else {
+        this._playButtonRootElement.style.left = `${left + canvasWidth / 2 - buttonWidth / 2}px`;
+        this._playButtonRootElement.style.top = `${top + canvasHeight / 2 - buttonHeight / 2 + 100}px`;
+      }
     }
 
     ctx.fillStyle = this.backgroundColor;
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-    const y = canvasHeight / 2;
+    let logoY = canvasHeight / 2;
     const width = Math.min(this.logoWidth, canvasWidth * 0.75);
-    const x = canvasWidth / 2 - width / 2;
+    let logoX = canvasWidth / 2 - width / 2;
+
+    if (this.logoPosition) {
+      logoX = this.logoPosition.x;
+      logoY = this.logoPosition.y;
+    }
 
     const imageHeight = Math.floor(width * (this.logoHeight / this.logoWidth)); // OG height/width factor
     const oldAntialias = this._engine.getAntialiasing();
     this._engine.setAntialiasing(true);
-    ctx.drawImage(this._image, 0, 0, this.logoWidth, this.logoHeight, x, y - imageHeight - 20, width, imageHeight);
+    if (!this.logoPosition) {
+      ctx.drawImage(this._image, 0, 0, this.logoWidth, this.logoHeight, logoX, logoY - imageHeight - 20, width, imageHeight);
+    } else {
+      ctx.drawImage(this._image, 0, 0, this.logoWidth, this.logoHeight, logoX, logoY, width, imageHeight);
+    }
 
     // loading box
     if (!this.suppressPlayButton && this._playButtonShown) {
@@ -326,13 +366,29 @@ export class Loader extends Class implements CanLoad {
       return;
     }
 
+    let loadingX = logoX;
+    let loadingY = logoY;
+    if (this.loadingBarPosition) {
+      loadingX = this.loadingBarPosition.x;
+      loadingY = this.loadingBarPosition.y;
+    }
+
     ctx.lineWidth = 2;
-    DrawUtil.roundRect(ctx, x, y, width, 20, 10);
+    DrawUtil.roundRect(ctx, loadingX, loadingY, width, 20, 10, this.loadingBarColor);
     const progress = width * (this._numLoaded / this._resourceCount);
     const margin = 5;
     const progressWidth = progress - margin * 2;
     const height = 20 - margin * 2;
-    DrawUtil.roundRect(ctx, x + margin, y + margin, progressWidth > 0 ? progressWidth : 0, height, 5, null, Color.White);
+    DrawUtil.roundRect(
+      ctx,
+      loadingX + margin,
+      loadingY + margin,
+      progressWidth > 10 ? progressWidth : 10,
+      height,
+      5,
+      null,
+      this.loadingBarColor
+    );
     this._engine.setAntialiasing(oldAntialias);
   }
 
