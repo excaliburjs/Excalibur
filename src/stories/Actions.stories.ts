9075ecebf3034f82d4e0db7f217c26e7c0eb3d4f
Kamran Ayub <kamran.ayub@gmail.com>
2020-05-03 15:03:05 -0500

    Add scale story

--- src/stories/Actions.stories.ts
+++ src/stories/Actions.stories.ts
@@ -43,7 +43,7 @@ export const fade: Story = withEngine(async (game) => {
 fade.story = {
   parameters: {
     componentSubtitle: 'Fade action',
-    docs: { storyDescription: 'Use `Actor.fade()` to fade in or out the Actor over time' }
+    docs: { storyDescription: 'Use `Actor.actions.fade()` to fade in or out the Actor over time' }
   }
 };
 
@@ -85,7 +85,7 @@ export const rotate: Story = withEngine(async (game) => {
 rotate.story = {
   parameters: {
     componentSubtitle: 'Rotate action',
-    docs: { storyDescription: 'Use `Actor.rotateBy()` or `Actor.rotateTo()` to rotate the Actor by an amount in radians' }
+    docs: { storyDescription: 'Use `Actor.actions.rotateBy()` or `Actor.actions.rotateTo()` to rotate the Actor by an amount in radians' }
   }
 };
 
@@ -112,21 +112,21 @@ export const move: Story = withEngine(async (game) => {
   heart.addDrawing(heartTx);
   game.add(heart);
 
-  // Rotate back and forth and loop
+  // Move back and forth and loop
   const originalPos = heart.pos.clone();
   heart.actions
 
-    // Rotate by an amount in radians
+    // Move by an amount in radians
     .moveBy(number('Move by x (px)', -100), number('Move by y (px)', 0), number('Duration (px/s)', 10))
     .delay(number('Pause', 2000))
-    // Rotate to a specific rotation
+    // Move to a specific rotation
     .moveTo(originalPos.x, originalPos.y, number('Duration (px/s)', 10));
 });
 
 move.story = {
   parameters: {
     componentSubtitle: 'Move actions',
-    docs: { storyDescription: 'Use `Actor.moveBy()` or `Actor.moveTo()` to move the Actor by an amount in pixels' }
+    docs: { storyDescription: 'Use `Actor.actions.moveBy()` or `Actor.actions.moveTo()` to move the Actor by an amount in pixels' }
   }
 };
 
@@ -153,18 +153,15 @@ export const ease: Story = withEngine(async (game) => {
   heart.addDrawing(heartTx);
   game.add(heart);
 
-  // Rotate back and forth and loop
+  // Ease back and forth and loop
   const originalPos = heart.pos.clone();
   heart.actions
-
-    // Rotate by an amount in radians
     .easeTo(
       originalPos.x + number('Ease by x (px)', -100),
-      originalPos.y + number('Move by y (px)', 0),
+      originalPos.y + number('Ease by y (px)', 0),
       number('Duration (ms)', 1000),
       EasingFunctions.EaseOutCubic
     )
-    // Rotate to a specific rotation
     .easeTo(originalPos.x, originalPos.y, number('Duration (ms)', 1000), EasingFunctions.EaseInOutCubic)
     .repeatForever();
 });
@@ -172,6 +169,47 @@ export const ease: Story = withEngine(async (game) => {
 ease.story = {
   parameters: {
     componentSubtitle: 'Ease actions',
-    docs: { storyDescription: 'Use `Actor.easeTo()` to ease the Actor by an amount in pixels with an easing function' }
+    docs: { storyDescription: 'Use `Actor.actions.easeTo()` to ease the Actor by an amount in pixels with an easing function' }
+  }
+};
+
+export const scale: Story = withEngine(async (game) => {
+  const heartTx = new Texture(heartTexture);
+  const loader = new Loader([heartTx]);
+
+  await game.start(loader);
+
+  game.setAntialiasing(false);
+
+  // Zoom in a bit
+  game.currentScene.camera.z = 4;
+
+  // Center the actor on the camera coordinates
+  const heart = new Actor({
+    x: game.currentScene.camera.x,
+    y: game.currentScene.camera.y,
+    width: 50,
+    height: 50
+  });
+
+  // Assign texture to heart actor
+  heart.addDrawing(heartTx);
+  game.add(heart);
+
+  // Scale back and forth and loop
+  const originalScale = heart.scale.clone();
+  heart.actions
+    .scaleBy(number('Scale by x factor', 2), number('Scale by y factor', 2), number('Speed (px/s)', 5))
+    .scaleTo(originalScale.x, originalScale.y, number('Speed (px/s)', 5), number('Speed (px/s)', 5))
+    .repeatForever();
+});
+
+scale.story = {
+  parameters: {
+    componentSubtitle: 'Scale actions',
+    docs: {
+      storyDescription:
+        'Use `Actor.actions.scaleBy()` and `Actor.actions.scaleTo()` to scale the Actor by a scale factor at a specific speed'
+    }
   }
 };
