abb4241482b20bddc11e786fc218054468285357
Erik Onarheim <erik.onarheim@gmail.com>
2020-07-02 16:08:49 -0500

    Another try

--- version.js
+++ version.js
@@ -20,7 +20,6 @@ function getCurrentCommit() {
 
 function getLatestTag(commit) {
   execSync('git fetch --depth=1000');
-  execSync('git fetch --unshallow');
   execSync('git fetch --all --tags');
   const tag = execSync(`git describe --tags ${commit} --abbrev=0`).toString().trim();
   return tag;
