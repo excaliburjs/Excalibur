e956620c840544edd6827e9c8ec312a6fe32bf77
Erik Onarheim <erik.onarheim@gmail.com>
2020-08-10 08:03:19 -0500

    fix: [#1547] Flakey tests (#1618)
    Maybe Closes #1547 multiple `done()` possibilities cause issues for Jasmine, and cause tests to fail sometimes
    
    ## Changes:
    
    - Fix Sound Tests
    - Fix Gif Tests
    - Fix Colorblind correction tests

--- src/spec/GifSpec.ts
+++ src/spec/GifSpec.ts
@@ -1,6 +1,5 @@
 import { ExcaliburMatchers, ensureImagesLoaded } from 'excalibur-jasmine';
 import * as ex from '@excalibur';
-import { Mocks } from './util/Mocks';
 import { TestUtils } from './util/TestUtils';
 
 describe('A Gif', () => {
@@ -42,22 +41,23 @@ describe('A Gif', () => {
 
       ensureImagesLoaded(engine.canvas, 'src/spec/images/GifSpec/frame1.png').then(([canvas, image]) => {
         expect(canvas).toEqualImage(image);
-        done();
-      });
-      engine.ctx.clearRect(0, 0, engine.canvas.width, engine.canvas.height);
 
-      sprite = gif.asSprite(1);
-      expect(gif.isLoaded()).toBe(true);
-      sprite.draw(engine.ctx, 0, 0);
-      ensureImagesLoaded(engine.canvas, 'src/spec/images/GifSpec/frame2.png').then(([canvas, image]) => {
-        expect(canvas).toEqualImage(image);
-        done();
+        engine.ctx.clearRect(0, 0, engine.canvas.width, engine.canvas.height);
+
+        sprite = gif.asSprite(1);
+        expect(gif.isLoaded()).toBe(true);
+        sprite.draw(engine.ctx, 0, 0);
+        ensureImagesLoaded(engine.canvas, 'src/spec/images/GifSpec/frame2.png').then(([canvas, image]) => {
+          expect(canvas).toEqualImage(image);
+          done();
+        });
       });
     });
   });
 
   it('should be read as a SpriteSheet', (done) => {
     gif.load().then(() => {
+      // jasmine.addMatchers(ExcaliburMatchers);
       expect(gif).toBeDefined();
       const spriteSheet: ex.SpriteSheet = gif.asSpriteSheet();
       const sprite = spriteSheet.getSprite(0);
