8017919121fd330ecd68629ce83da871c4e86e44
Erik Onarheim <erik.onarheim@gmail.com>
2021-12-03 20:45:45 -0600

    fix test with pre-mult blending

--- src/engine/Graphics/Context/shaders/point-fragment.glsl
+++ src/engine/Graphics/Context/shaders/point-fragment.glsl
@@ -9,7 +9,7 @@ void main() {
   float r = 0.0, delta = 0.0, alpha = 1.0;
   vec2 cxy = 2.0 * gl_PointCoord - 1.0;
   r = dot(cxy, cxy);
-  
+
 #ifdef GL_OES_standard_derivatives
   delta = fwidth(r);
   alpha = 1.0 - smoothstep(1.0 - delta, 1.0 + delta, r);
@@ -18,16 +18,7 @@ void main() {
     discard;
   }
 #endif
-  
-  // blend the alphas
-  // vec3 final_color = v_color.rgb * fract(v_color.a);
-  // vec4 black = vec4(0.0, 0.0, 0.0, alpha);
-  // float final_alpha = alpha + black.a * (1.0 - alpha);
-  // gl_FragColor = vec4((black.rgb * black.a + final_color * (1.0 - black.a)) / final_alpha, final_alpha);
-  // // float final_alpha = v_color.a + alpha * (1.0 = v_color.a);
-  // // final_color =  final_color.rgb * alpha + final_color.rgb * (1.0 - alpha);
-  // // float final_alpha = alpha + 
-
+  gl_FragColor = vec4(v_color.rgb, v_color.a * alpha);
   // "premultiply" the color by alpha
-  gl_FragColor = vec4(v_color.rgb * v_color.a * alpha, v_color.a * alpha);
+  gl_FragColor.rgb *= gl_FragColor.a;
 }
