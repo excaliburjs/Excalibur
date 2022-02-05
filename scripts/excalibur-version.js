78676c8af763c228417e0610c7c3ffc4e4db6221
Erik Onarheim <erik.onarheim@gmail.com>
2022-02-04 20:49:39 -0600

    chore: Update release automation (#2230)
    Preparing for v0.25.3, updating release automation to improve quality of life

--- scripts/excalibur-version.js
+++ scripts/excalibur-version.js
@@ -1,5 +1,10 @@
 const replace = require('replace-in-file');
-const version = require('../version').getCiVersion();
+const versioner = require('../version');
+let version = versioner.getAlphaVersion();
+if (process.env.release) {
+  version = versioner.getReleaseVersion();
+}
+console.log('Replacing __EX_VERSION with:', version);
 const options = {
   files: 'build/dist/index.js',
   from: /process\.env\.__EX_VERSION/g,
