98405ea825b042593ad1fdaf3451dd019bfc3eef
Erik Onarheim <erik.onarheim@gmail.com>
2022-05-01 17:32:09 -0500

    fix: Reproduce non-unique id issue

--- sandbox/tests/triangulation/index.ts
+++ sandbox/tests/triangulation/index.ts
@@ -35,6 +35,7 @@ game.add(actor);
 var actor2 = new ex.Actor({x: 400, y: 200, collisionType: ex.CollisionType.Active});
 actor2.collider.set(ex.Shape.Box(100, 100).tessellate());
 actor2.graphics.use(new ex.Rectangle({ width: 100, height: 100, color: ex.Color.Black}));
+actor2.rotation = (Math.PI / 2) * 3;
 game.add(actor2);
 
 
