c82bf71253f649b256c8a4008c6e741b93fea70e
Erik Onarheim <erik.onarheim@gmail.com>
2022-06-25 15:58:00 -0500

    update comment

--- src/spec/_boot.ts
+++ src/spec/_boot.ts
@@ -4,7 +4,7 @@ ex.Flags.enable('suppress-obsolete-message');
 const testsContext = require.context('.', true, /Spec$/);
 testsContext.keys().forEach(testsContext);
 
-// Wild hack to avoid karma from locking up
+// Wild hack to prevent karma from locking up
 // https://github.com/jasmine/jasmine/issues/1327#issuecomment-300095516
 beforeEach(function(done) {
   window.setTimeout(function() {
