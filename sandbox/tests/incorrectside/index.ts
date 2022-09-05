3719bd6962645ea724fe2f3feed13e2fcf5317e3
Erik Onarheim <erik.onarheim@gmail.com>
2022-09-05 17:10:15 -0500

    fix: [#2473] ignore 0 sized precollisions (#2474)
    Closes #2473
    
    This PR fixes an issue where a degenerate 0 sized collision causes issues with the precollision event emit. Previously an erroneous left precollision would be emitted when the minimum translation vector (mtv) was `(-0, -0)`
    
    <img width="299" alt="image" src="https://user-images.githubusercontent.com/612071/187574499-96889716-ba1a-418e-995e-b7673ce643e2.png">
    
    
    ## Changes:
    
    - Filters out 0 sized contacts in precollision to guard against a degenerate side calculation

---
+++ sandbox/tests/incorrectside/index.ts
@@ -0,0 +1,71 @@
+var engine = new ex.Engine({
+  width: 400,
+  height: 400
+});
+
+ex.Physics.gravity = ex.vec(0, 800);
+
+class Player4 extends ex.Actor {
+  collisions = [];
+
+  constructor() {
+    super({
+      name: "player",
+      x: 200,
+      y: 200,
+      width: 40,
+      height: 40,
+      color: ex.Color.Red,
+      collisionType: ex.CollisionType.Active
+    });
+
+    this.on("precollision", (ev) => this.onPreCollision(ev));
+  }
+
+  onPreCollision(ev) {
+    this.collisions.push(ev.side);
+  }
+
+  onPreUpdate() {
+    console.log(this.collisions);
+    this.collisions = [];
+
+    if (engine.input.keyboard.wasPressed(ex.Input.Keys.ArrowUp)) {
+      this.vel.y = -400;
+    }
+
+    if (engine.input.keyboard.isHeld(ex.Input.Keys.ArrowLeft)) {
+      this.vel.x = -100;
+    } else if (engine.input.keyboard.isHeld(ex.Input.Keys.ArrowRight)) {
+      this.vel.x = 100;
+    } else {
+      this.vel.x = 0;
+    }
+  }
+}
+
+engine.start().then(() => {
+  engine.add(new Player4());
+
+  engine.add(
+    new ex.Actor({
+      y: 300,
+      anchor: ex.vec(0, 0),
+      width: 400,
+      height: 100,
+      color: ex.Color.Green,
+      collisionType: ex.CollisionType.Fixed
+    })
+  );
+  engine.add(
+    new ex.Actor({
+      x: 300,
+      y: 220,
+      anchor: ex.vec(0, 0),
+      width: 40,
+      height: 80,
+      color: ex.Color.Green,
+      collisionType: ex.CollisionType.Fixed
+    })
+  );
+});
