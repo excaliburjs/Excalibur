6e31014d12098aebd365f2ee0428f564d4a87858
Kamran Ayub <kamran.ayub@gmail.com>
2023-10-30 14:32:49 -0500

    wip getting started

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -137,6 +137,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          sidebarCollapsed: false,
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
