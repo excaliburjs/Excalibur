13a85ebd06a77922eee8b8356d9849f583887e5d
Kamran Ayub <kamran.ayub@gmail.com>
2023-11-11 00:12:23 -0600

    undo: storybook example

--- site/src/components/docs/Example.tsx
+++ site/src/components/docs/Example.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import useBaseUrl from '@docusaurus/useBaseUrl'
-import { usePluginData } from '@docusaurus/useGlobalData';
 
 const FRAME_STYLE = {
   border: '1px solid #aaa',
@@ -12,15 +10,13 @@ const FRAME_STYLE = {
  * @prop story The story ID in the URL to navigate to
  */
 export default function Example({ story = '' }) {
-  const { devServerAddress, basePath } = usePluginData('storybook-plugin') as { devServerAddress?: string; basePath: string; };
-  const buildUrl = useBaseUrl(`/${basePath}/?nav=0&path=/docs/${story}`);
-  const devUrl = `${devServerAddress}?nav=0&path=/docs/${story}`;
-  http://localhost:3000/examples/iframe.html?args=&id=audio--playing-a-sound
+  const url = `/examples/?nav=0&path=/docs/${story}`;
+
   return (
     <iframe
       title="Excalibur Storybook Example"
       allowFullScreen
-      src={devServerAddress ? devUrl : buildUrl}
+      src={url}
       frameBorder={0}
       width="100%"
       height="600"
