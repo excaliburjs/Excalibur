fec2ab941cba4741bb867f2d3c6941df587bdce0
Erik Onarheim <erik.onarheim@gmail.com>
2023-11-17 22:53:18 -0600

    docs: fix build

--- site/src/pages/premium-support/index.js
+++ site/src/pages/premium-support/index.js
@@ -2,7 +2,26 @@ import styles from './styles.module.css';
 import Layout from '@theme/Layout';
 import clsx from 'clsx';
 
-import offerings from './data';
+const data = [
+  {
+    title: '1 Hour of Excalibur Support',
+    link: 'https://savvycal.com/excalibur/support-1-hour',
+    description: 'Book 1 hour of dedicated time with an Excalibur core maintainer. We will meet over a video call to discuss and work to provide you the information, issue resolution, debugging, or custom development you need.',
+    price: '$100'
+  },
+  {
+    title: '2 Hours of Excalibur Support',
+    link: 'https://savvycal.com/excalibur/support-2-hour',
+    description: 'Book 2 hours of dedicated time with an Excalibur core maintainer. We will meet over a video call to discuss and work to provide you the information, issue resolution, debugging, or custom development you need.',
+    price: '$180'
+  },
+  {
+    title: 'Custom Development Project',
+    link: 'https://savvycal.com/excalibur/custom-quote',
+    description: 'Schedule a free 30 minute meeting to get a quote for your project. This work would involve a maintainer writing custom code for your game, either in Excalibur, building a new plugin, or directly in your code base.',
+    price: 'Starts at $2000'
+  }
+]
 
 const list = (...array) => {
   return array.join(' ')
@@ -56,7 +75,7 @@ export default function Support() {
             </ul>
           </p>
 
-          <OfferingList items={offerings} />
+          <OfferingList items={data} />
 
         </div>
       </main>
