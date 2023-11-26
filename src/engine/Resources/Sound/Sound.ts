ee42ce2ea745d78a2f02ad8475d5f9563d60f971
Erik Onarheim <erik.onarheim@gmail.com>
2023-11-25 19:04:12 -0600

    fix: bug where incorrect track was removed when stop/start in rapid succession

--- src/engine/Resources/Sound/Sound.ts
+++ src/engine/Resources/Sound/Sound.ts
@@ -228,6 +228,10 @@ export class Sound implements Audio, Loadable<AudioBuffer> {
     return this._tracks.some(t => t.isPaused());
   }
 
+  public isStopped(): boolean {
+    return this._tracks.some(t => t.isStopped());
+  }
+
   /**
    * Play the sound, returns a promise that resolves when the sound is done playing
    * An optional volume argument can be passed in to play the sound. Max volume is 1.0
@@ -355,7 +359,9 @@ export class Sound implements Audio, Loadable<AudioBuffer> {
    * Starts playback, returns a promise that resolves when playback is complete
    */
   private async _startPlayback(): Promise<boolean> {
-    const track = await this._getTrackInstance(this.data);
+    const track = this._getTrackInstance(this.data);
+
+    // this._tracks.push(track);
 
     const complete = await track.play(() => {
       this.events.emit('playbackstart', new NativeSoundEvent(this, track));
@@ -363,8 +369,14 @@ export class Sound implements Audio, Loadable<AudioBuffer> {
     });
 
     // when done, remove track
-    this.events.emit('playbackend', new NativeSoundEvent(this, track));
-    this._tracks.splice(this.getTrackId(track), 1);
+    const doneTracks = this._tracks.filter(t => t.isStopped());
+    doneTracks.forEach((track: WebAudioInstance) => {
+      this.events.emit('playbackend', new NativeSoundEvent(this, track));
+    })
+    // this._tracks.splice(this.getTrackId(track), 1);
+
+    // cleanup any done tracks
+    this._tracks = this._tracks.filter(t => !t.isStopped());
 
     return complete;
   }
