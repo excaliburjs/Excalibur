b7da05a7a4dad7407429b0bb07ae91984c61fee9
Erik Onarheim <erik.onarheim@gmail.com>
2023-12-07 20:21:34 -0600

    remove facebook link

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -255,7 +255,7 @@ const config: Config = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus'
+              href: 'https://github.com/excaliburjs/Excalibur'
             }
           ]
         }
