53e6a9500bc89ac9de71b8b498ed31ed2e0ce601
Erik Onarheim <erik.onarheim@gmail.com>
2024-01-14 22:09:21 -0600

    Attempt cutover

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://beta.excaliburjs.com',
+  url: 'https://excaliburjs.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
