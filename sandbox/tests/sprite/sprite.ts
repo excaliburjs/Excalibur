8a3b4e226a52fcb6b3700be771bc8e40522760cc
Erik Onarheim <erik.onarheim@gmail.com>
2024-02-01 15:29:48 -0600

    Update sprite example

--- sandbox/tests/sprite/sprite.ts
+++ sandbox/tests/sprite/sprite.ts
@@ -4,54 +4,13 @@ var game = new ex.Engine({
   canvasElementId: 'game',
   width: 600,
   height: 400,
-  displayMode: ex.DisplayMode.FitScreenAndFill
+  displayMode: ex.DisplayMode.FitScreenAndFill,
+  pixelArt: true,
+  // antialiasing: false
 });
 
-var fragmentSource = `#version 300 es
-precision mediump float;
 
-// UV coord
-in vec2 v_uv;
-
-uniform sampler2D u_graphic;
-
-uniform vec2 u_graphic_resolution;
-
-out vec4 fragColor;
-
-vec2 v2len(in vec2 a, in vec2 b) {
-  return sqrt(a*a+b*b);
-}
-
-vec4 textureBlocky(in sampler2D tex, in vec2 uv, in vec2 res) {
-  uv *= res; // enter texel coordinate space.
-  
-  vec2 seam = floor(uv+.5); // find the nearest seam between texels.
-  
-  // here's where the magic happens. scale up the distance to the seam so that all
-  // interpolation happens in a one-pixel-wide space.
-  uv = (uv-seam)/v2len(dFdx(uv),dFdy(uv))+seam;
-  
-  uv = clamp(uv, seam-.5, seam+.5); // clamp to the center of a texel.
-  
-  return texture(tex, uv/res, -1000.);//.xxxx; // convert back to 0..1 coordinate space.
-}
-
-void main() {
-  fragColor = textureBlocky(u_graphic, v_uv, u_graphic_resolution);
-}
-`
-
-var material = game.graphicsContext.createMaterial({
-  name: 'aa',
-  fragmentSource
-});
-
-var tex = new ex.ImageSource(
-  'https://cdn.rawgit.com/excaliburjs/Excalibur/7dd48128/assets/sword.png',
-  false,
-  ex.ImageFiltering.Blended
-  );
+var tex = new ex.ImageSource('https://cdn.rawgit.com/excaliburjs/Excalibur/7dd48128/assets/sword.png');
 
 var loader = new ex.Loader([tex]);
 
@@ -65,7 +24,6 @@ actor.onInitialize = () => {
     }
   });
   actor.graphics.add(sprite);
-  actor.graphics.material = material;
 };
 game.add(actor);
 game.start(loader);
