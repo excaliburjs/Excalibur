83ad416290c192d751f7c222de4e12a4f085d12c
Matt Jennings <me@mattjennings.io>
2024-02-16 18:34:12 -0700

    feat: ex.FontSource resource type

---
+++ src/engine/Resources/Font.ts
@@ -0,0 +1,75 @@
+import { Font } from "../Graphics/Font"
+import { FontOptions } from "../Graphics/FontCommon"
+import { GraphicOptions, RasterOptions } from "../Graphics"
+import { Loadable } from "../Interfaces/Loadable"
+import { Resource } from "./Resource"
+
+export interface FontSourceOptions
+  extends Omit<FontOptions, 'family'>,
+    GraphicOptions,
+    RasterOptions {
+  bustCache?: boolean
+}
+
+export class FontSource implements Loadable<FontFace> {
+  private _resource: Resource<Blob>
+  private _isLoaded = false
+  private path: string
+  private options: FontSourceOptions
+
+  data!: FontFace
+
+  /**
+   * The font family name
+   */
+  family: string
+
+  constructor(
+    path: string,
+    family: string,
+    { bustCache, ...options }: FontSourceOptions = {}
+  ) {
+    this.path = path
+    this._resource = new Resource(path, 'blob', bustCache)
+    this.options = options
+    this.family = family
+  }
+
+  async load(): Promise<FontFace> {
+    if (this.isLoaded()) {
+      return this.data
+    }
+
+    try {
+      const blob = await this._resource.load()
+      const url = URL.createObjectURL(blob)
+
+      const fontFile = new FontFace(this.family, `url(${url})`)
+      
+      document.fonts.add(fontFile)      
+
+      await fontFile.load()
+      this.data = fontFile
+      this._isLoaded = true
+    } catch (error) {
+      throw `Error loading FontSource from path '${this.path}' with error [${
+        (error as Error).message
+      }]`
+    }
+    return this.data
+  }
+
+  isLoaded(): boolean {
+    return this._isLoaded
+  }
+
+  /**
+   * Build a font from this FontSource. 
+   * 
+   * @param options {FontOptions} Override the font options
+   */
+  toFont(options?: FontOptions): Font {
+    return new Font({ family: this.family, ...this.options, ...options })
+  }
+
+}
