b0e026c2df8d814f5aedb9ebb20f9611a78a4958
Erik Onarheim <erik.onarheim@gmail.com>
2024-03-25 23:06:38 -0500

    add test

--- src/spec/GraphicsGroupSpec.ts
+++ src/spec/GraphicsGroupSpec.ts
@@ -47,6 +47,43 @@ describe('A Graphics Group', () => {
     await expectAsync(canvasElement).toEqualImage('src/spec/images/GraphicsGroupSpec/graphics-group.png');
   });
 
+  it('can be created and drawn without anchor', async () => {
+    const rect1 = new ex.Rectangle({
+      width: 25,
+      height: 25,
+      color: ex.Color.Blue
+    });
+
+    const rect2 = new ex.Rectangle({
+      width: 25,
+      height: 25,
+      color: ex.Color.Yellow
+    });
+
+    const group = new ex.GraphicsGroup({
+      useAnchor: false,
+      members: [
+        { offset: ex.vec(0, 0), graphic: rect1 },
+        { offset: ex.vec(25, 25), graphic: rect2 }
+      ]
+    });
+
+    expect(group.width).toBe(50);
+    expect(group.height).toBe(50);
+    expect(group.localBounds.width).toBe(50);
+    expect(group.localBounds.height).toBe(50);
+
+    const canvasElement = document.createElement('canvas');
+    canvasElement.width = 100;
+    canvasElement.height = 100;
+    const ctx = new ex.ExcaliburGraphicsContext2DCanvas({ canvasElement });
+
+    ctx.clear();
+    group.draw(ctx, 100, 100);
+
+    await expectAsync(canvasElement).toEqualImage('src/spec/images/GraphicsGroupSpec/graphics-group-without-anchor.png');
+  });
+
   it('can be cloned', () => {
     const animation = new ex.Animation({
       frames: []
