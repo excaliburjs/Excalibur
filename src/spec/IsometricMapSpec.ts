baf6016f3a50144ead66be3dd7b78381fa807fe9
Erik Onarheim <erik.onarheim@gmail.com>
2024-03-28 20:36:18 -0500

    add tests

--- src/spec/IsometricMapSpec.ts
+++ src/spec/IsometricMapSpec.ts
@@ -301,4 +301,39 @@ describe('A IsometricMap', () => {
     expect(sut.getTileByPoint(ex.vec(250, 234)).y).toBe(14);
   });
 
+  it('can respond to pointer events', async () => {
+    const engine = TestUtils.engine({width: 100, height: 100});
+    await TestUtils.runToReady(engine);
+    const sut = new ex.IsometricMap({
+      pos: ex.vec(100, 100),
+      tileWidth: 64,
+      tileHeight: 48,
+      rows: 20,
+      columns: 20
+    });
+    engine.add(sut);
+
+    const tile = sut.getTile(0, 0);
+
+    const pointerdown = jasmine.createSpy('pointerdown');
+    const pointerup = jasmine.createSpy('pointerup');
+    const pointermove = jasmine.createSpy('pointermove');
+    const pointercancel = jasmine.createSpy('pointercancel');
+    tile.on('pointerdown', pointerdown);
+    tile.on('pointerup', pointerup);
+    tile.on('pointermove', pointermove);
+    tile.on('pointercancel', pointercancel);
+
+    engine.input.pointers.triggerEvent('down', ex.vec(110, 110));
+    engine.input.pointers.triggerEvent('up', ex.vec(110, 110));
+    engine.input.pointers.triggerEvent('move', ex.vec(110, 110));
+    engine.input.pointers.triggerEvent('cancel', ex.vec(110, 110));
+    expect(pointerdown).toHaveBeenCalledTimes(1);
+    expect(pointerup).toHaveBeenCalledTimes(1);
+    expect(pointermove).toHaveBeenCalledTimes(1);
+    expect(pointercancel).toHaveBeenCalledTimes(1);
+
+    engine.dispose();
+  });
+
 });
