3f3ac09f0eac25adfe8243533fe95771a8d85220
Erik Onarheim <erik.onarheim@gmail.com>
2024-03-29 23:39:36 -0500

    perf: 2x image draw perf - remove allocations from image-renderer (#2962)
    See excalibur bunnymark https://github.com/excaliburjs/excalibur-bunnymark
    
    This PR removes allocations and makes several perf improvements that DOUBLES our performance on the bunnymark
    
    * Removes all allocations from the draw image hot path
    * Removes allocations from object pool
    * Removes unecessary getters
    * Speed up AffineMatrix clone
    * Switches webgl to VAOs
    * Speed up texture id lookup
    * Caches native thunks to width/height
    
    ## New Perf Results
    
     ~50fps @ 20,000 sprites.
    
    ![image](https://github.com/excaliburjs/Excalibur/assets/612071/292a5ff5-4e03-4d19-9118-a2f3455960c1)
    
    ~30fps @ 35000 sprites
    
    ![image](https://github.com/excaliburjs/Excalibur/assets/612071/8abbb13b-cfcf-4b38-9d89-dc5def385d11)
    
    
    ## Previous Perf Results on Excalibur main 
    
    
    ~25fps @ 20000 sprites
    
    ![image](https://github.com/excaliburjs/Excalibur/assets/612071/afe0bab7-b2ee-4edf-a3c0-d9ede0325cb5)
    
    
    ~15fps @ 35000 sprites
    ![image](https://github.com/excaliburjs/Excalibur/assets/612071/391bb767-e712-494a-b442-fd7425af8637)

--- src/engine/Math/affine-matrix.ts
+++ src/engine/Math/affine-matrix.ts
@@ -263,6 +263,33 @@ export class AffineMatrix {
     }
   }
 
+  /**
+   * Packed array of length 8, that contains 4 vertices, with 2 components each
+   * So: [x0, y0, x1, y1, x2, y2, x3, y3]
+   * @param quad
+   */
+  multiplyQuadInPlace(quad: number[]) {
+    const resultTopLeftX = quad[0] * this.data[0] + quad[1] * this.data[2] + this.data[4];
+    const resultTopLeftY = quad[0] * this.data[1] + quad[1] * this.data[3] + this.data[5];
+    quad[0] = resultTopLeftX;
+    quad[1] = resultTopLeftY;
+
+    const resultTopRightX = quad[2] * this.data[0] + quad[3] * this.data[2] + this.data[4];
+    const resultTopRightY = quad[2] * this.data[1] + quad[3] * this.data[3] + this.data[5];
+    quad[2] = resultTopRightX;
+    quad[3] = resultTopRightY;
+
+    const resultBottomLeftX = quad[4] * this.data[0] + quad[5] * this.data[2] + this.data[4];
+    const resultBottomLeftY = quad[4] * this.data[1] + quad[5] * this.data[3] + this.data[5];
+    quad[4] = resultBottomLeftX;
+    quad[5] = resultBottomLeftY;
+
+    const resultBottomRightX = quad[6] * this.data[0] + quad[7] * this.data[2] + this.data[4];
+    const resultBottomRightY = quad[6] * this.data[1] + quad[7] * this.data[3] + this.data[5];
+    quad[6] = resultBottomRightX;
+    quad[7] = resultBottomRightY;
+  }
+
   to4x4() {
     const mat = new Matrix();
     mat.data[0] = this.data[0];
@@ -383,18 +410,11 @@ export class AffineMatrix {
   }
 
   /**
-   * Creates a new Matrix with the same data as the current 4x4
+   * Creates a new Matrix with the same data as the current [[AffineMatrix]]
    */
   public clone(dest?: AffineMatrix): AffineMatrix {
     const mat = dest || new AffineMatrix();
-    mat.data[0] = this.data[0];
-    mat.data[1] = this.data[1];
-
-    mat.data[2] = this.data[2];
-    mat.data[3] = this.data[3];
-
-    mat.data[4] = this.data[4];
-    mat.data[5] = this.data[5];
+    mat.data.set(this.data);
     return mat;
   }
 
