12a8b237d58589a718e56b55d8ab63ebe22848f0
Erik Onarheim <erik.onarheim@gmail.com>
2024-04-07 14:24:28 -0500

    improve pointer perf by keeping majority local

--- src/engine/Collision/Colliders/PolygonCollider.ts
+++ src/engine/Collision/Colliders/PolygonCollider.ts
@@ -497,13 +497,16 @@ export class PolygonCollider extends Collider {
   public contains(point: Vector): boolean {
     // Always cast to the right, as long as we cast in a consistent fixed direction we
     // will be fine
-    const testRay = new Ray(point, new Vector(1, 0));
-    const intersectCount = this.getSides().reduce(function (accum, side) {
+    const localPoint = this._transform.applyInverse(point);
+    const testRay = new Ray(localPoint, new Vector(1, 0));
+
+    let intersectCount = 0;
+    const sides = this.getLocalSides();
+    for (let side of sides) {
       if (testRay.intersect(side) >= 0) {
-        return accum + 1;
+        intersectCount++
       }
-      return accum;
-    }, 0);
+    }
 
     if (intersectCount % 2 === 0) {
       return false;
