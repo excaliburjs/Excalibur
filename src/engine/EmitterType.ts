f05e62dd33aeb854ac9d87fa53537ac424a1a78c
Pascal Garber <pascal@artandcode.studio>
2024-05-10 11:51:51 +0200

    fix: Cirular dependency on Actor

---
+++ src/engine/EmitterType.ts
@@ -0,0 +1,13 @@
+/**
+ * An enum that represents the types of emitter nozzles
+ */
+export enum EmitterType {
+    /**
+     * Constant for the circular emitter type
+     */
+    Circle,
+    /**
+     * Constant for the rectangular emitter type
+     */
+    Rectangle
+}
