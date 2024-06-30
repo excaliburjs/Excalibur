43e4215f2ad7c6d92cb76c19492f7fbc7d9d8d75
Erik Onarheim <erik.onarheim@gmail.com>
2024-06-30 14:47:41 -0500

    fix formatting

--- src/engine/Util/Util.ts
+++ src/engine/Util/Util.ts
@@ -107,16 +107,16 @@ export function isObject(item: any): item is object {
  */
 export function mergeDeep<T extends object>(target: T, ...sources: T[]) {
   if (!sources.length) {
-return target;
-}
+    return target;
+  }
   const source = sources.shift();
 
   if (isObject(target) && isObject(source)) {
     for (const key in source) {
       if (isObject(source[key])) {
         if (!target[key]) {
-Object.assign(target, { [key]: {} });
-}
+          Object.assign(target, { [key]: {} });
+        }
         mergeDeep(target[key] as any, source[key]);
       } else {
         Object.assign(target, { [key]: source[key] });
