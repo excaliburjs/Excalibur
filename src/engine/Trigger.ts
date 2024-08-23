07ff860fe95505caecdaffe89160b7844e361b60
Torick <yorickvanklinken@gmail.com>
2024-08-23 08:11:44 +0200

    Boy Scout Rule applied

--- src/engine/Trigger.ts
+++ src/engine/Trigger.ts
@@ -1,4 +1,3 @@
-import { Engine } from './Engine';
 import { Vector } from './Math/vector';
 import { ExitTriggerEvent, EnterTriggerEvent, CollisionEndEvent, CollisionStartEvent } from './Events';
 import { CollisionType } from './Collision/CollisionType';
@@ -38,18 +37,6 @@ export interface TriggerOptions {
   repeat: number;
 }
 
-const triggerDefaults: Partial<TriggerOptions> = {
-  pos: Vector.Zero,
-  width: 10,
-  height: 10,
-  visible: false,
-  action: () => {
-    return;
-  },
-  filter: () => true,
-  repeat: -1
-};
-
 /**
  * Triggers are a method of firing arbitrary code on collision. These are useful
  * as 'buttons', 'switches', or to trigger effects in a game. By default triggers
@@ -57,72 +44,57 @@ const triggerDefaults: Partial<TriggerOptions> = {
  */
 export class Trigger extends Actor {
   public events = new EventEmitter<TriggerEvents & ActorEvents>();
-  private _target: Entity;
+  public target?: Entity;
   /**
    * Action to fire when triggered by collision
    */
-  public action: (entity: Entity) => void = () => {
-    return;
-  };
+  public action: (entity: Entity) => void;
   /**
    * Filter to add additional granularity to action dispatch, if a filter is specified the action will only fire when
    * filter return true for the collided entity.
    */
-  public filter: (entity: Entity) => boolean = () => true;
+  public filter: (entity: Entity) => boolean;
   /**
    * Number of times to repeat before killing the trigger,
    */
-  public repeat: number = -1;
+  public repeat: number;
 
   /**
-   * @param opts Trigger options
+   * @param options Trigger options
    */
-  constructor(opts: Partial<TriggerOptions>) {
-    super({ x: opts.pos.x, y: opts.pos.y, width: opts.width, height: opts.height });
-    opts = {
-      ...triggerDefaults,
-      ...opts
-    };
+  constructor(options: Partial<TriggerOptions>) {
+    super({ x: options.pos?.x, y: options.pos?.y, width: options.width, height: options.height });
 
-    this.filter = opts.filter || this.filter;
-    this.repeat = opts.repeat || this.repeat;
-    this.action = opts.action || this.action;
-    if (opts.target) {
-      this.target = opts.target;
-    }
+    this.filter = options.filter ?? (() => true);
+    this.repeat = options.repeat ?? -1;
+    this.action = options.action ?? (() => undefined);
+    this.target = options.target;
 
-    this.graphics.visible = opts.visible;
+    this.graphics.visible = options.visible ?? false;
     this.body.collisionType = CollisionType.Passive;
 
-    this.events.on('collisionstart', (evt: CollisionStartEvent<Entity>) => {
-      if (this.filter(evt.other)) {
-        this.events.emit('enter', new EnterTriggerEvent(this, evt.other));
-        this._dispatchAction(evt.other);
-        // remove trigger if its done, -1 repeat forever
-        if (this.repeat === 0) {
-          this.kill();
-        }
+    this.events.on('collisionstart', ({ other: collider }: CollisionStartEvent<Entity>) => {
+      if (!this._matchesTarget(collider)) {
+        return;
+      }
+
+      this.events.emit('enter', new EnterTriggerEvent(this, collider));
+      this._dispatchAction(collider);
+      // remove trigger if its done, -1 repeat forever
+      if (this.repeat === 0) {
+        this.kill();
       }
     });
 
-    this.events.on('collisionend', (evt: CollisionEndEvent<Entity>) => {
-      if (this.filter(evt.other)) {
-        this.events.emit('exit', new ExitTriggerEvent(this, evt.other));
+    this.events.on('collisionend', ({ other: collider }: CollisionEndEvent<Entity>) => {
+      if (this._matchesTarget(collider)) {
+        this.events.emit('exit', new ExitTriggerEvent(this, collider));
       }
     });
   }
 
-  public set target(target: Entity) {
-    this._target = target;
-    this.filter = (entity: Entity) => entity === target;
-  }
-
-  public get target() {
-    return this._target;
-  }
-
-  public _initialize(engine: Engine) {
-    super._initialize(engine);
+  private _matchesTarget(entity: Entity): boolean {
+    return this.filter(entity) && (this.target === undefined || this.target === entity);
   }
 
   private _dispatchAction(target: Entity) {
