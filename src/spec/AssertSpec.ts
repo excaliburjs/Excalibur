ca3e7eee9dc0c5502e831baad349657b8ae71d31
Erik Onarheim <erik.onarheim@gmail.com>
2024-12-01 21:30:19 -0600

    fix test

--- src/spec/AssertSpec.ts
+++ src/spec/AssertSpec.ts
@@ -12,24 +12,6 @@ describe('An assert', () => {
     expect(action).toThrowError('throws');
   });
 
-  it('will not throw in prod when expression is false', () => {
-    globalThis.process = {
-      env: {
-        NODE_ENV: 'production'
-      }
-    };
-    const action = () => {
-      ex.assert('throws', () => false);
-    };
-
-    expect(action).toThrowError('throws');
-    globalThis.process = {
-      env: {
-        NODE_ENV: 'development'
-      }
-    };
-  });
-
   it('will not throw in dev when expression is true', () => {
     const action = () => {
       ex.assert('throws', () => true);
