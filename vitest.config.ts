8c53f793b5c2879fe460ee3e68d78182dfcfdef6
Matt Jennings <me@mattjennings.io>
2025-03-04 23:31:17 -0600

    disable gpu again

--- vitest.config.ts
+++ vitest.config.ts
@@ -67,9 +67,9 @@ export default defineConfig({
               '--mute-audio',
               '--no-sandbox',
               '--enable-precise-memory-info',
-              '--js-flags="--max_old_space_size=8192"'
+              '--js-flags="--max_old_space_size=8192"',
               // breaks webGL on arm macs
-              // !isArmMacOS && '--disable-gpu'
+              !isArmMacOS && '--disable-gpu'
             ].filter(Boolean)
           }
         },
