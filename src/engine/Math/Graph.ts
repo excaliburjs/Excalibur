5e8c7d218bbd510b134c508c4858515c79ede94c
Justin Young <justin_dean_young@yahoo.com>
2025-04-02 16:17:24 -0400

    updated Math.random() to Random

--- src/engine/Math/Graph.ts
+++ src/engine/Math/Graph.ts
@@ -1,3 +1,4 @@
+import { Random } from './Random';
 import { Vector } from './vector';
 
 /**
@@ -792,9 +793,11 @@ export type Vertex<T> = Node<T>;
 
 class GraphUUId {
   static generateUUID(): G_UUID {
+    const rng = new Random();
     const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+
     const generatedUuid = uuid.replace(/[xy]/g, function (c) {
-      const r = (Math.random() * 16) | 0;
+      const r = (rng.nextInt() * 16) | 0;
       const v = c === 'x' ? r : (r & 0x3) | 0x8;
       return v.toString(16);
     });
