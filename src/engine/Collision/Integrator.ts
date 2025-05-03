6ce9b5d7dd7037f8d18e83511bc21b061a007e77
Kyle <kfalicov@gmail.com>
2025-05-03 15:14:53 -0400

    feat: Add maxVel to MotionComponent

--- src/engine/Collision/Integrator.ts
+++ src/engine/Collision/Integrator.ts
@@ -1,6 +1,7 @@
 import { Vector } from '../Math/vector';
 import { TransformComponent } from '../EntityComponentSystem';
 import { MotionComponent } from '../EntityComponentSystem/Components/MotionComponent';
+import { clamp } from '../Math';
 
 export class EulerIntegrator {
   // Scratch vectors to avoid allocation
@@ -17,6 +18,12 @@ export class EulerIntegrator {
     // This code looks a little wild, but it's to avoid creating any new Vector instances
     // integration is done in a tight loop so this is key to avoid GC'ing
     motion.vel.addEqual(totalAcc.scale(seconds, EulerIntegrator._ACC));
+    // clamp the components of the velocity vector
+    motion.vel.setTo(
+      clamp(motion.vel.x, -motion.maxVel.x, motion.maxVel.x),
+      clamp(motion.vel.y, -motion.maxVel.y, motion.maxVel.y)
+    );
+
     transform.pos
       .add(motion.vel.scale(seconds, EulerIntegrator._VEL), EulerIntegrator._POS)
       .addEqual(totalAcc.scale(0.5 * seconds * seconds, EulerIntegrator._VEL_ACC));
