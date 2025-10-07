5269d4a74a5186dc5f18d94e8902ed6e82eceda7
Chris Kelly <chris@workinggears.com>
2025-10-08 09:17:03 +1100

    docs: create taller embed variant for playground iframes

--- site/src/components/docs/IFrameEmbed.tsx
+++ site/src/components/docs/IFrameEmbed.tsx
@@ -1,12 +1,24 @@
 import React, { IframeHTMLAttributes } from 'react'
 
-const DEFAULT_FRAME_STYLE = {
-  width: '100%',
-  height: '420px',
-  border: 0,
-  overflow: 'hidden',
+const DEFAULT_FRAME_STYLES = {
+  regular: {
+    width: '100%',
+    height: '420px',
+    border: 0,
+    overflow: 'hidden',
+  },
+  playground: {
+    width: '100%',
+    height: '600px',
+    border: 0,
+    overflow: 'hidden',
+  },
+} as const
+
+type Props = IframeHTMLAttributes<HTMLIFrameElement> & {
+  variant: 'regular' | 'playground'
 }
 
-export default ({ src, ...props }: IframeHTMLAttributes<HTMLIFrameElement>) => (
-  <iframe src={src} style={DEFAULT_FRAME_STYLE} {...props}></iframe>
+export default ({ src, variant, ...props }: Props) => (
+  <iframe src={src} style={DEFAULT_FRAME_STYLES[variant]} {...props}></iframe>
 )
