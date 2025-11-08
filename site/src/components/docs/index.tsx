e76eafae0bc6a10854340fd01b19e60878b3e6d7
Chris Kelly <chris@workinggears.com>
2025-11-08 15:00:49 +1100

    spike: typecheck playground samples

--- site/src/components/docs/index.tsx
+++ site/src/components/docs/index.tsx
@@ -2,3 +2,4 @@ export { default as Example } from "./Example";
 export { default as CodeSandboxEmbed } from './CodeSandboxEmbed';
 export { default as IFrameEmbed } from "./IFrameEmbed";
 export { default as GameCodeBlock } from './GameCodeBlock';
+export { default as PlaygroundEmbed } from './PlaygroundEmbed';
