bc6ab760931bd40ed7a5f25a3190d75ef1ed91fa
Kamran Ayub <kamran@hey.com>
2025-12-02 14:02:15 -0600

    fix(tests): compatibility with VS Code Vitest extension (#3612)

--- vitest.config.ts
+++ vitest.config.ts
@@ -28,7 +28,7 @@ export default defineConfig(
         reporter: [['html'], ['lcov', { projectRoot: __dirname }], ['text-summary']],
         reportsDirectory: path.join(__dirname, 'coverage')
       },
-      projects: ['./src/spec/vitest.config.unit.ts', './src/spec/vitest.config.visual.ts']
+      projects: [path.resolve(__dirname, './src/spec/vitest.config.unit.ts'), path.resolve(__dirname, './src/spec/vitest.config.visual.ts')]
     }
   } satisfies ViteUserConfig)
 );
