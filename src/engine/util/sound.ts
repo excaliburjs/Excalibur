cd62c3d8ba555fee50294f5b6d1b56388c8a7dd3
Erik Onarheim <erik.onarheim@gmail.com>
2025-12-29 10:31:58 -0600

    better regex

--- src/engine/util/sound.ts
+++ src/engine/util/sound.ts
@@ -6,7 +6,7 @@ import { Logger } from './log';
 export function canPlayFile(file: string): boolean {
   try {
     const a = new Audio();
-    const filetype = /.*\.([A-Za-z0-9]+)(?:(?:\?|\#).*)*$/;
+    const filetype = /.*\.([A-Za-z0-9]+)(?=[?#]|$)/;
     const type = file.match(filetype)[1];
     if (a.canPlayType('audio/' + type)) {
       return true;
