43e68630299fcfa3107a10ed5b1c0d75ec8935d6
Chris Kelly <chris@workinggears.com>
2026-06-19 22:46:00 +1000

    chore: fix vite formatting

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: process.env.BASE_URL || './',
